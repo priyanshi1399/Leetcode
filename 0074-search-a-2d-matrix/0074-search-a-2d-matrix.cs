83a407a936e40fd7bb0d7f20579489e0bc1d79ad
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-09-09 04:42:15 +0530

    Time: 78 ms (41.30%), Space: 43.2 MB (53.85%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cs
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cs
@@ -1,15 +1,26 @@
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
-        bool b=false;
-        for(int i=0;i<matrix.Length;i++){
-            for(int j=0;j<matrix[i].Length;j++){
-                if(matrix[i][j]==target){
-                    b=true;
-                    break;
-                }
+        int n=matrix.Length;
+        int m=matrix[0].Length;
+        int low=0;
+        int high=(n*m-1);
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            int row=mid/m;
+            int col=mid%m;
+
+            if(matrix[row][col]==target){
+                return true;
             }
+            else if(matrix[row][col]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+
         }
-        return b;
-        
+        return false;
     }
 }
