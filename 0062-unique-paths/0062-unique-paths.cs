8572717ae864b4dd42552d26d03720984c62b08c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-09-26 23:59:54 +0530

    Time: 18 ms (80.55%), Space: 26.4 MB (99.20%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cs
+++ 0062-unique-paths/0062-unique-paths.cs
@@ -1,3 +1,43 @@
+// public class Solution {
+
+// public static int CountPath(int i,int j,int m,int n,int [,] matrix){
+
+//     if(i>=m || j>=n){
+//         return 0;
+//     }
+//     if(i==m-1 && j==n-1){
+//         return 1;
+//     }
+//     if(matrix[i,j]!=-1){
+//         return matrix[i,j]; //suppose checking for [1,1] and already [1,1] is marked to 1 then no need to check again
+//     }
+//     else{
+
+//     matrix[i,j]=CountPath(i+1,j,m,n,matrix)+ CountPath(i,j+1,m,n,matrix);
+//     return matrix[i,j];
+//     }
+// }
+
+//     public int UniquePaths(int m, int n) {
+        
+//         int[,] matrix=new int[m,n];
+
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 matrix[i,j]=-1;
+//             }
+//         }
+
+//         int num=CountPath(0,0,m,n,matrix);
+
+//         if(m==1 && n==1){
+//             return num;
+//         }
+//         return matrix[0,0];
+//     }
+// }
+//This is brute force involving DP
+
 public class Solution {
     public int UniquePaths(int m, int n) {
         int N=n+m-2;
