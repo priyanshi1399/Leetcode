0d74038c5998c3af9d9d44b7cc41d1cb9ce9429c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-06 10:39:11 +0530

    Time: 1 ms (42.84%), Space: 40.9 MB (27.89%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {  
+        /*if(n<=0)
+            return false;     
+       return (n &(n-1))==0;
+*/
+
+
+
+//for 0 need to check because 0 and 0-1
+//isPowerofTwo(3)
+//8 & 7=1000 &  0111=0
+//4 &3 =100 &011=0
+//n & n-1==0--> n is power of Two
+//6 & 5=110 & 101
+//1______& 01111=0
+//n & n-1==0-->n is power of 2
+
+
+//2nd Method
+
+    if(n<=0)
+        return false;
+    while(n>0){
+        if(n==1) //n==3
+            return true;
+        if(n%2!=0)
+            return false;
+        n=n/2;
+    }
+    return false;
+    }
+
+    //16--8-->4-->2-->1
+   // 3-->
+}
