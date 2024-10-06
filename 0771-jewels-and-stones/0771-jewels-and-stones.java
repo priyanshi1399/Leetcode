98bab7d11707654b63a0000e199472e83699dbf6
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-06 16:47:13 +0530

    Time: 1 ms (54.29%), Space: 41.6 MB (63.09%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,13 +1,21 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int count=0;
-        for(int i=0;i<stones.length();i++){ //i==0
+        /*for(int i=0;i<stones.length();i++){ //i==0
            for(int j=0;j<jewels.length();j++){ //j==0
             if(stones.charAt(i)==jewels.charAt(j)){
                 count++; //count=1 a,A
             }
            }
         }
-        return count;
+        return count; */
+
+        for(int i=0;i<stones.length();i++){
+            if(jewels.contains(String.valueOf(stones.charAt(i)))){ //here contains ethod expecting a char sequence
+            //like string so we have to give String.valueOf to give argument as String
+                count++;
+            }
+        }
+        return count; 
     }
 }
