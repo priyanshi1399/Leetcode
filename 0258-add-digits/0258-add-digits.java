e1d2dd39022d4bc302a354a4279e7a8f058ccaf1
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-08 12:22:38 +0530

    Time: 0 ms (100.00%), Space: 41 MB (23.92%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,24 +1,11 @@
 class Solution {
-    
-   
     public int addDigits(int num) {
-       while(countNumberofDigit(num)>1){
-        num=num1;
-       }
-       return num1;
-
-    }
-     int num1=0;
-    public int countNumberofDigit(int num){
-         int sum=0;
-        int count=0;
-        while(num!=0){
-            int rem=num%10;
-            sum=sum+rem;
-            num=num/10;
-            count++;
+        if(num==0){
+            return 0;
         }
-        num1=sum;
-        return count;
+        if(num%9==0){
+            return 9;
+        }
+        return (num%9);
     }
 }
