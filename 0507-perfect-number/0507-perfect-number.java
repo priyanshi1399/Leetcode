6bf99b13234df763c3641ef27dff037402ce28f8
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-17 01:44:56 +0530

    Time: 2086 ms (58.45%), Space: 40.3 MB (43.22%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        if(num==1){
+            return false;
+        }
+        int sum=0;
+        for(int i=1;i<=num/2;i++){
+            if(num%i==0){
+                sum=sum+i;
+            }
+        }
+        if(sum==num){
+            return true;
+        }
+        return false;
+    }
+}
