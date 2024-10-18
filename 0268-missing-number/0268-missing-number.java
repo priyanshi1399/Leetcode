f84651d3565b3c70126100749b98475a18238d0c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-19 03:10:25 +0530

    Time: 6 ms (13.17%), Space: 44.8 MB (91.68%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,25 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n=nums.length; 
-        int result=n; //storing length
-    //XOR of same number will be zero 
-        for(int i=0;i<n;i++){
-            result=result^i;
-            result=result^nums[i];
+        int n=nums.length;
+        Arrays.sort(nums);
+        int result=nums.length;
+        int l=0;
+        int r=n-1; //at last
+
+        while(l<=r){
+        //calculating mid
+        int mid=l+(r-l)/2;
+
+        if(nums[mid]>mid) //element should be on left or may be my mid is element
+        {
+            result=mid;
+            r=mid-1;
         }
-        return result;
+        else{
+            l=mid+1;
+        }
+        
     }
+    return result;
+}
 }
