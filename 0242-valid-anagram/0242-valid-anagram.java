84e2febaa872caddab9c227bfc4b1ac130fd99df
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-22 03:12:39 +0530

    Time: 2175 ms (5.56%), Space: 43 MB (75.42%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false; //base case
+        }
+        char [] s1=s.toCharArray();
+        char [] t1=t.toCharArray();
+        int count=0;
+        for(int i=0;i<t1.length;i++){
+            for(int j=0;j<s1.length;j++){
+                if(t1[i]==s1[j]){
+                    if(s1[j]!='#'){
+                        s1[j]='#';
+                        count++;
+                        break; //it will be out of j and start with i again from 0 in s1
+                    }
+                    else if(s1[j]=='#') {//character encountered before
+                    continue; //increasing i
+                    }
+                }
+            }
+        }
+        if(count==s.length()){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
+//TC(0(n^2))->two loops
+//S(C)=0(n)
