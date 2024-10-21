c0dccb89e96badd24d47f03c351f4f240dce3f7d
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-21 09:21:56 +0530

    Time: 2 ms (86.03%), Space: 46.1 MB (20.97%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,6 +1,28 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
+        int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+            j=i; //getting index of first zero o(n)
+            break;
+            }
+        }
+        if(j==-1) //there are no zeroes
+            return;
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0) {//checking if it is non zero putting i after j then move both i and j
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++; //one condition is that also when at j there 
+            //is 0 then also i will be moving everytime so not written else
+            }
+        }
+        
+        //TC--0(x)+o(n-x)-0(n)
+
+       /* int n=nums.length;
         int [] res=new int[n];
         int j=0;
         for(int i=0;i<n;i++){
@@ -11,6 +33,6 @@ class Solution {
         }
         for(int i=0;i<res.length;i++){
             nums[i]=res[i];
-        }
+        }*/
     }
 }
