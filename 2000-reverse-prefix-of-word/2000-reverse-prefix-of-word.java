da13f4cdcf298dbae9a8b516dbfa29d027dfc367
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-22 15:50:33 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (97.72%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -23,6 +23,30 @@ class Solution {
             j--;
         }
        return new String(chars); 
+
+
+       /*
+         char [] charArray=word.toCharArray();
+        StringBuilder part=new StringBuilder();
+        int n=charArray.length;
+        String result="";
+        for(int i=0;i<n;i++){
+            if(charArray[i]==ch){
+                String s1=word.substring(0,i+1);
+                part.append(s1);
+                part.reverse();
+                String s2=word.substring(i+1,n);
+                result=part.toString()+s2;
+                break;
+            }           
+        }
+        if(result==""){
+        return word;
+        }
+        else{
+            return result;
+        }     
+       */
         
     }
 }
