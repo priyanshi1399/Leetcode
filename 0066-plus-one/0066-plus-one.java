b3a292056f5c02e713905cbd94d408856a7247d1
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-26 02:17:32 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (5.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,20 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-    for(int i=digits.length-1;i>=0;i--){
+        int n=digits.length;
+           for(int i=n-1;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0; //[9] ==>first [0] then create one more length below [1,0];
+            }
+            else{
+                digits[i]++; //one time addition
+                return digits;
+            }
+           }
+           digits=new int[digits.length+1];
+           digits[0]=1;
+           return digits;
+
+    /*for(int i=digits.length-1;i>=0;i--){
         if(digits[i]==9){
             digits[i]=0; //setting as 0 after this loop increase size and add arr[0];
         }
@@ -11,6 +25,6 @@ class Solution {
     }
     digits=new int[digits.length+1]; //0 , 0, 0 ,0=>for 9,9 ,9
     digits[0]=1;
-    return digits;
+    return digits;*/
     }
 }
