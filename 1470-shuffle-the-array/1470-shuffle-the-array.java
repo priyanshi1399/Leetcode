f23e0d78c30b9a31e518f0cfc5a92de021fa2e3a
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-26 08:57:51 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (60.52%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,11 +1,40 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int len=nums.length;
-        int [] res=new int[len];
-        for(int i=0;i<n;i++){
-            res[i*2]=nums[i];
-            res[2*i+1]=nums[n+i];
+
+        int [] res=new int [2*n];
+        int i=0;
+        int j=n;
+        int k=0;
+        while(k<2*n){
+            res[k++]=nums[i++];
+            res[k++]=nums[j++];
         }
         return res;
     }
 }
+/*
+[2,5,1,3,4,7]
+i=0->1-->2-->3
+j=3->4
+k=0-->1-->3
+
+res[0]=2
+res[1]=3
+res[2]=5
+res[3]=4
+res[4]=1
+res[5]=7
+
+now k=6 and 6<2*3 wrong 
+
+*/
+
+        /*int k=nums.length;
+        int [] res=new int[k];
+        for(int i=0;i<n;i++){
+            res[2*i]=nums[i];
+            res[2*i+1]=nums[n+i];
+        }
+        return res;
+    }
+}*/
