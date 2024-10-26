6e8d84ee0f00d465cd7e35e6a85a34ac8c46184b
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-27 03:50:06 +0530

    Time: 2 ms (99.77%), Space: 42.6 MB (87.89%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,6 +1,57 @@
 class Solution {
+
+    public void reverseString(char [] arr,int start,int end){
+        while(start<end){
+            char temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
     public String reverseWords(String s) {
+
+        int n=s.length();
+        int start=0;
+        int end=0;
         int i=0;
+        int j=0;
+
+        char [] chars=s.toCharArray();
+        
+        reverseString(chars,0,n-1); //task-1 is to reverse the array
+
+        //iterate j
+        while(j<n){
+
+            while(j<n && chars[j]==' '){ //till space increase j
+                j++;
+            }
+            start=i; //give your start or update your start
+
+            while(j<n && chars[j]!=' '){ //character found and goes till end of that whole characters "hello"
+                chars[i]=chars[j]; //put j in i means shifting to the left
+                i++;
+                j++;
+            }
+            end=i-1;
+
+            //reverse those characters
+            reverseString(chars,start,end);
+
+            if(j<n){ //we are not on end
+            chars[i++] = ' '; //giving just one space no other space
+             //j is on there only because again we go to while incraese j and repeats the steps
+            }
+        }
+        if(chars[i-1]==' '){
+            i=i-1;
+        }
+        String s3=new String(chars).substring(0,i).toString();
+        return s3;
+
+       /* int i=0;
         int n=s.length();
         int start=0;
         int end=0;
@@ -29,6 +80,6 @@ class Solution {
             ans.append(' ');
         }
        }
-       return ans.toString();
+       return ans.toString();*/
     }
 }
