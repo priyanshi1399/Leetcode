915edcc7358ec14af3855eb2a0770e3859249255
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-27 13:45:10 +0530

    Time: 2 ms (65.69%), Space: 44.1 MB (85.63%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,5 +1,23 @@
 class Solution {
+
     public int buyChoco(int[] prices, int money) {
+       /* int n=prices.length;
+        int min=Integer.MAX_VALUE;
+       for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){ 
+            int sum=0;
+            sum=sum+prices[i]+prices[j];
+            if(sum<min){
+                min=sum;
+            }         
+        }
+    }
+            if(min<=money){
+                return money-min;
+            }
+        
+        return money;*/
+
         Arrays.sort(prices);
         int sum=prices[0]+prices[1];
         if(sum<=money){
