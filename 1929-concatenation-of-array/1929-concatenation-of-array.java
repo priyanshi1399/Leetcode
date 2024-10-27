bdef1e1056436b55385354661079913f0922d02a
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-27 09:14:45 +0530

    Time: 1 ms (97.72%), Space: 45.5 MB (18.76%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -2,6 +2,12 @@ class Solution {
     public int[] getConcatenation(int[] nums) {
         int n=nums.length;
         int [] res=new int[2*n];
+        //nums=[2,7,9]
+        //res=[2,7,9,2,7,9]
+        //res[0]=res[3]=nums[0];
+        //res[1]=res[4]=nums[1]
+        //res[2]=res[5]=nums[2]
+        //res[i]=res[i+n]=nums[i]
         for(int i=0;i<n;i++){
             res[i]=nums[i];
             res[n+i]=nums[i];
