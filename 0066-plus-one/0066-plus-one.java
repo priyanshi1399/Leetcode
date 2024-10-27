ad3578483e3d03c5d4054a33403940c62db40b04
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-27 09:40:58 +0530

    Time: 0 ms (100.00%), Space: 42 MB (10.00%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,30 +1,41 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n=digits.length;
-           for(int i=n-1;i>=0;i--){
-            if(digits[i]==9){
-                digits[i]=0; //[9] ==>first [0] then create one more length below [1,0];
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i]++;
+                break;
             }
             else{
-                digits[i]++; //one time addition
+                digits[i]=0;
+            }
+        }
+        if(digits[0]==0){
+            int [] res=new int[digits.length+1];
+            res[0]=1;
+            return res;
+        }
+        return digits;
+
+/*
+89->80->90
+1899->1890-->1800-->1900
+99-->90->00->if first bit is 0-.increasing size of array
+*/
+       /* int n=digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
                 return digits;
             }
-           }
-           digits=new int[digits.length+1];
-           digits[0]=1;
-           return digits;
-
-    /*for(int i=digits.length-1;i>=0;i--){
-        if(digits[i]==9){
-            digits[i]=0; //setting as 0 after this loop increase size and add arr[0];
+            else if(digits[i]==9){
+                digits[i]=0;
+            }
         }
-        else{
-            digits[i]++; //one time addition 
-            return digits;
-        }
-    }
-    digits=new int[digits.length+1]; //0 , 0, 0 ,0=>for 9,9 ,9
-    digits[0]=1;
-    return digits;*/
+        digits=new int[digits.length+1];
+        digits[0]=1;
+        return digits;*/
     }
 }
+//123-->124
+//199-->200
+//999->1000 this is the special case where you have to create new array
