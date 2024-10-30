2b8450a05f6519cdf3e965bae6f7f7402e79d1fc
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-10-30 08:18:02 +0530

    Time: 4 ms (60.16%), Space: 45.8 MB (6.06%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.java
@@ -1,22 +1,25 @@
 class Solution {
 
-    public int [] generateArray(int k,int s,int [] res){
-        int [] newArray=new int[res.length+k];
-        System.arraycopy(res,0,newArray,0,res.length);
-
-       for(int i=0;i<k;i++){
-        newArray[res.length+i]=s;
+    public ArrayList<Integer> appendToList(int freq,int val,ArrayList<Integer> arrlst){
+       
+        for(int i=0;i<freq;i++){
+            arrlst.add(val);
         }
-    return newArray;
+        return arrlst;
     }
 
+
     public int[] decompressRLElist(int[] nums) {
-      //  int [] ans=new int[0];
-        int [] res=new int[0];
         int n=nums.length;
+         ArrayList<Integer> lst=new ArrayList<>();
         for(int i=0;i<n;i=i+2){
-            res=generateArray(nums[i],nums[i+1],res);
+        lst=appendToList(nums[i],nums[i+1],lst);
         }
-        return res;
+        int [] res=new int[lst.size()];
+        for(int i=0;i<res.length;i++){
+            res[i]=lst.get(i);
+        }
+
+    return res;
     }
 }
