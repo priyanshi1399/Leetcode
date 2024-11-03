b87ccedebccd03ff674cc16e6ee35c3aebc6ca4c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-11-03 22:58:54 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (87.39%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,6 +1,35 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l=0;
+        //first figure out the upper bound and then find the index of the element
+        int n=nums.length;
+         int l=0;
+         int h=n;
+        while(l<h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]<=target){
+                l=mid+1;
+            }
+            else{
+                h=mid;
+            }
+        }
+        /*
+       int upperBound=l;
+       if(upperBound-1>=0 && nums[upperBound-1]==target){
+        return upperBound-1;
+       }
+       else{
+        return -1;
+       }*/
+       int lowerBound=l-1;
+       if(lowerBound>=0 && nums[lowerBound]==target){
+        return lowerBound;
+       }
+       else{
+        return -1;
+       }
+
+       /* int l=0;
         int h=nums.length-1;
         while(l<=h){
             int mid=l+(h-l)/2;
@@ -14,7 +43,7 @@ class Solution {
                 l=mid+1;
             }
         }
-        return -1;
+        return -1;*/
 
         /*for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
