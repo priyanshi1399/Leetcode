87621faf674ac3ccf494a8687753de8826821ad8
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-11-05 21:43:12 +0530

    Time: 0 ms (100.00%), Space: 42 MB (52.63%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,6 +1,30 @@
 class Solution {
      public boolean searchMatrix(int [][]matrix, int target){
-        int m=matrix.length;
+
+        if(matrix==null || matrix.length<1 || matrix[0].length<1){
+            return false;
+        }
+        if(matrix.length==1 && matrix[0].length==1 && target==matrix[0][0]){
+            return true;
+        }
+        int r=0;
+        int c=matrix[0].length-1;
+
+        while(c>=0 && r<=matrix.length-1){
+            if(target==matrix[r][c]){
+                return true;
+            }
+            else if(target<matrix[r][c]){
+                    c--;
+            }
+            else if(target>matrix[r][c]){
+                r++;
+            }
+        }
+            return false;
+        
+
+        /*int m=matrix.length;
         int n=matrix[0].length;
 
         int l=0;
@@ -19,7 +43,7 @@ class Solution {
                 h=mid-1;
             }          
         }
-        return false;
+        return false; */
 
 
 
