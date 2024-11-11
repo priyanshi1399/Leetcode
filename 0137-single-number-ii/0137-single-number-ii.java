ea00b86131de09ce6d7f2eec35da411122e8a88b
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-11-11 06:36:57 +0530

    Time: 4 ms (57.88%), Space: 45.2 MB (81.35%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,6 +1,14 @@
 class Solution {
     public int singleNumber(int[] nums) {
-    
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=1;i<n;i=i+3){
+            if(nums[i]!=nums[i-1]){
+                return nums[i-1];
+            }
+        }
+        return nums[n-1];
+    /*
         int ans=0;
         for(int bitIndex=0;bitIndex<32;bitIndex++){
             int count_ith=0;
@@ -13,6 +21,6 @@ class Solution {
                 ans=ans | (1<<bitIndex);
             }
         }
-        return ans;
+        return ans;*/
     }
 }
