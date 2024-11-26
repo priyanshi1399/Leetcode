545db31942436ffe60ca3ba16149dc9aa401da5c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-11-26 21:21:11 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (23.33%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.java
@@ -1,6 +1,6 @@
 class Solution {
     public int minCostToMoveChips(int[] position) {
-        int n=position.length;
+     /*   int n=position.length;
         int count1=0;
         int count2=0;
         for(int i=0;i<n;i++){
@@ -11,6 +11,18 @@ class Solution {
                 count2++;
             }
         }
-        return Math.min(count1,count2);
+        return Math.min(count1,count2);*/
+        int n=position.length;
+        int even=0;
+        int odd=0;
+        for(int p:position){
+            if(p%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        return Math.min(even,odd);
     }
 }
