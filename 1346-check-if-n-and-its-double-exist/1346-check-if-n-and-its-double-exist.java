71d02af647857d6da45cb4c49e22e558c471501b
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-12-02 05:09:52 +0530

    Time: 14 ms (6.13%), Space: 44.7 MB (5.84%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,7 +1,24 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
 
-        int n=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i]*2)){
+                return true;
+            }
+            else if(arr[i]%2==0 && map.containsKey(arr[i]/2)){
+                return true;
+            }
+            else{
+            map.put(arr[i],1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            System.out.println(entry.getKey() + ":"+ entry.getValue());
+        }
+        return false;
+
+       /* int n=arr.length;
         int i=0;
         while(i<n){
         for(int j=0;j<n;j++){
@@ -11,7 +28,8 @@ class Solution {
         }
         i++;
         }
-        return false;
+        return false;*/
+
        /* int n=arr.length;
         for(int i=0;i<n;i++){
             int j=0;
