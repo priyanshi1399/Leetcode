99ccb7a2906c24ced244a361fe75c5c7e6e6b30c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-12-01 19:59:15 +0530

    Time: 2 ms (93.52%), Space: 43.4 MB (17.04%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,6 +1,18 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
+
         int n=arr.length;
+        int i=0;
+        while(i<n){
+        for(int j=0;j<n;j++){
+            if(i!=j && arr[i]==2*arr[j]){
+                return true;
+            }
+        }
+        i++;
+        }
+        return false;
+       /* int n=arr.length;
         for(int i=0;i<n;i++){
             int j=0;
             while(j<n){
@@ -12,6 +24,6 @@ class Solution {
                 }
             }
         }
-        return false;
+        return false;*/
     }
 }
