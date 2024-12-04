57e299893f85b40d7237df96033851a78ca1ec32
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-12-04 16:00:36 +0530

    Time: 13 ms (63.58%), Space: 45.4 MB (19.12%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,11 +1,24 @@
 class Solution {
-    private int calculateBanana(int [] piles, int mid){
+   /* private int calculateHours(int [] piles, int mid){
         int sum=0;
         for(int i=0;i<piles.length;i++){         
            int res = (int)Math.ceil((double)piles[i]/mid);
             sum=sum+res;
         }
         System.out.println(sum);
+        return sum;
+    }*/
+
+    private int calculateHours(int [] piles, int mid){
+        int sum=0;
+        for(int i=0;i<piles.length;i++){         
+          if(piles[i]%mid==0){
+            sum+=piles[i]/mid;
+          }
+          else{
+            sum+=piles[i]/mid+1;
+          }
+        }
         return sum;
     }
 
@@ -15,8 +28,8 @@ class Solution {
         int r=Arrays.stream(piles).max().getAsInt();
         while(l<r){
             int mid=l+(r-l)/2;
-            int s=calculateBanana(piles,mid);
-            if(s<=h){
+            int totalHours=calculateHours(piles,mid);
+            if(totalHours<=h){
                 r=mid;
             }
             else{
