84edecfbbf919d5cebb0486410161424504990a7
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-12-15 17:57:24 +0530

    Time: 3 ms (53.57%), Space: 42 MB (22.96%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,6 +1,34 @@
 class Solution {
     public int calPoints(String[] operations) {
+        Stack<Integer> stck=new Stack<>();
         int n=operations.length;
+        for(int i=0;i<n;i++){
+            if(operations[i].equals("C")){
+                stck.pop();
+            }
+            else if(operations[i].equals("D")){
+                stck.push(2*stck.peek());
+            }
+            else if(operations[i].equals("+")){
+                int first=stck.pop();
+                int second=stck.pop();
+                stck.push(second); //we popped this in second  time so we will push first
+                stck.push(first); //we popped first then we will put above second in stck
+                stck.push(first+second); //above that need to push this sum of 2 previous sum
+            }
+            else{
+                stck.push(Integer.parseInt(operations[i]));
+            }
+        }
+
+        int sum=0;
+        while(!stck.isEmpty()){
+            sum=sum+stck.pop();
+        }
+        return sum;
+
+
+      /*  int n=operations.length;
         int j=0;
         int result[]=new int[n];
         for(int i=0;i<n;i++){        
@@ -26,6 +54,6 @@ class Solution {
         for(int i=0;i<result.length;i++){
             sum+=result[i];
         }
-        return sum;
+        return sum;*/
     }
 }
