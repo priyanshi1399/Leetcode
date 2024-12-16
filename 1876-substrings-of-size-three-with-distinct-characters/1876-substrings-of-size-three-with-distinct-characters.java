6a66a5d662fde690df25ea5e56478c4610b24de0
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-12-16 20:19:16 +0530

    Time: 60 ms (5.99%), Space: 45.2 MB (5.40%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,6 +1,36 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
+        if(s.length()<3){
+            return 0;
+        }
+       HashMap<Character,Integer> map=new HashMap<>();
+       int count=0;
+        int subCount=0;
+
+       for(int i=0;i<s.length();i++){
+        count++;
+        map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        
+    
+        if((count==3) &&(map.size()==3)){
+            for(Map.Entry<Character,Integer> ent:map.entrySet()){
+            System.out.println(ent.getKey()+ ":"+ ent.getValue());
+            }
+            subCount++;
+            count=0;
+            map.clear();
+            i=i-3+1;
+        }
+        else if((count==3) && (map.size()<3)){
+            count=0;
+            map.clear();
+            i=i-3+1;
+        }
+       
+       }
+       return subCount;
+
+        /*HashMap<Character,Integer> map=new HashMap<>();
         int count=0;
         int n=s.length();
         for(int i=0;i<=n-3;i++){
@@ -14,6 +44,6 @@ class Solution {
         }
         map.clear();
     }
-    return count;
+    return count;*/
 }
 }
