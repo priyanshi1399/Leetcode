9c19bdbdaebdb4e39a839aeccac17f00ddddca95
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-12-17 14:22:04 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (40.77%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -1,6 +1,37 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int n=code.length;
+        int [] res=new int[n];
+        if(k==0){
+            return res;
+        }
+        int left=(k>0)?1:n+k;
+        int right=(k>0)?k:n-1;
+        int sum=0;
+        for(int i=left;i<=right;i++){
+            sum+=code[i%n];
+        }
+        /* 5 7
+         1 4 k=-2 n=4
+        left =2 right=3
+        sum=1+4=5
+         i=0
+         res[0]=5
+         sum=5-code[(2+0)%4]  sum=5-1=4
+        sum=4+code[(3+0+1)%4] sum=4+5=9
+        i=1          
+        res[1]=9
+        
+        */
+        for(int i=0;i<n;i++){
+            res[i]=sum;
+            sum-=code[(left+i)%n];
+            sum+=code[(right+i+1)%n];
+        }
+        return res;
+
+        /*
+        int n=code.length;
         int [] arr=new int[n];
         int sum=0;
         if(k>0){
@@ -46,5 +77,8 @@ class Solution {
             }
         }
         return arr;
+
+
+        */
     }
 }
