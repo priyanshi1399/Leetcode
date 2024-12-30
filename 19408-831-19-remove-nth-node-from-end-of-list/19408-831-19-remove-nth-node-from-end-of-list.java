d8adf74960d1d54ad209cb7cad5cf9bfc6efc971
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2024-12-31 05:02:05 +0530

    Time: 0 ms, Memory: 42 MB - LeetHub

--- 19408-831-19-remove-nth-node-from-end-of-list/19408-831-19-remove-nth-node-from-end-of-list.java
+++ 19408-831-19-remove-nth-node-from-end-of-list/19408-831-19-remove-nth-node-from-end-of-list.java
@@ -10,6 +10,27 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast=head;
+        ListNode slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        while(fast!=null && fast.next!=null){
+            fast=fast.next;
+            slow=slow.next;
+
+        }
+        if(fast==null){
+            return head.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
+
+/*Brute Force
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head==null){
             return null;
         }
@@ -40,3 +61,6 @@ class Solution {
         return head;
     }
 }
+
+
+*/
