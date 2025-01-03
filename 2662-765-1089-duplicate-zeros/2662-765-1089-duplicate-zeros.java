f95b753e7052fd91a8f69867e23646b96f8acab6
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-01-03 06:41:24 +0530

    Time: 1 ms, Memory: 44.8 MB - LeetHub

--- 2662-765-1089-duplicate-zeros/2662-765-1089-duplicate-zeros.java
+++ 2662-765-1089-duplicate-zeros/2662-765-1089-duplicate-zeros.java
@@ -1,11 +1,35 @@
 class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n=arr.length;
+        int zeroes=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                zeroes++;
+            }
+        }
 
-    public void shiftArrayValues(int [] arr,int k){
+        for(int i=n-1;i>=0;i--){
+            if(zeroes+i<n){
+                arr[i+zeroes]=arr[i];
+            }
+            if(arr[i]==0){
+                zeroes--;
+                if(i+zeroes<n){
+                    arr[i+zeroes]=0;
+                }
+            }
+        }
+    }
+}
+
+
+/*
+ public void shiftArrayValues(int [] arr,int k){
         int n1=arr.length;
         for(int i=n1-1;i>k;i--){
             arr[i]=arr[i-1];
         }
-        if(k!=n1){     //it should not be inserting at last    
+        if(k!=n1){       
         arr[k]=0;
         }      
     }
@@ -18,4 +42,7 @@ class Solution {
             }
         }
     }
-}
+
+
+
+*/
