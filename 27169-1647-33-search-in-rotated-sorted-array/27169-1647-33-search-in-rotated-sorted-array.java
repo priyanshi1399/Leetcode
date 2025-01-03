969c1e020240febd3dad90a1c1d464aa99f16385
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-01-03 15:46:47 +0530

    Time: 0 ms, Memory: 42.5 MB - LeetHub

--- 27169-1647-33-search-in-rotated-sorted-array/27169-1647-33-search-in-rotated-sorted-array.java
+++ 27169-1647-33-search-in-rotated-sorted-array/27169-1647-33-search-in-rotated-sorted-array.java
@@ -1,6 +1,36 @@
 class Solution {
+    public int search(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
 
-    public int customBinarySearch(int l,int r,int [] nums,int target){
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[l]){
+                if(nums[l]<=target && target<=nums[mid]){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[r]){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+/* 0(n)
+
+ public int customBinarySearch(int l,int r,int [] nums,int target){
         
         while(l<=r){
         int mid=l+(r-l)/2;
@@ -43,35 +73,7 @@ class Solution {
         }
         
     }
-}
-/*
- public int search(int[] nums, int target) {
-        int l=0;
-        int r=nums.length-1;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(nums[mid]==target){
-                return mid;
-            } // 6 7 0 1 2 4 5
-            else if(nums[mid]>=nums[l]){
-                if(nums[l]<=target && target<=nums[mid]){
-                    r=mid-1;
-                }
-                else{
-                    l=mid+1;
-                }
-            }
-            else{
-                if(nums[mid]<=target && target<=nums[r]){
-                    l=mid+1;
-                }
-                else{
-                    r=mid-1;
-                }
-            }
 
-        }
-        return -1;
-    }
+
 
 */
