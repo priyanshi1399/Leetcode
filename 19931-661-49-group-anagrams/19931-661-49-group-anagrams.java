1f88b2997a25c2ecadd09be8ca8fee611b25356c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-01-14 05:32:42 +0530

    Time: 9 ms, Memory: 48 MB - LeetHub

--- 19931-661-49-group-anagrams/19931-661-49-group-anagrams.java
+++ 19931-661-49-group-anagrams/19931-661-49-group-anagrams.java
@@ -1,5 +1,37 @@
 class Solution {
-    public String letterConcatenate(int frequency, char ch){
+
+    public  String appendAfterCounting(String str){
+        int [] arr=new int[26];
+        for(int i=0;i<str.length();i++){
+            arr[str.charAt(i)-'a']++;
+        }
+        StringBuilder builder=new StringBuilder();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+            builder.append(String.valueOf((char)(i+'a')).repeat(arr[i]));
+            }
+        }
+        return builder.toString();       
+
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map=new HashMap<>();
+        for(String s: strs){
+        String newString= appendAfterCounting(s);
+
+        if(!map.containsKey(newString)){
+            map.put(newString,new ArrayList<>());
+        }
+        map.get(newString).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
+
+
+
+    /*public String letterConcatenate(int frequency, char ch){
         String appended="";
         for(int i=0;i<frequency;i++){
             appended=appended+ch;
@@ -35,4 +67,4 @@ class Solution {
 
         return new ArrayList<>(map.values());
     }
-}
+}*/
