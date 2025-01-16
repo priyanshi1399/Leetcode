37079004d1774c8bc674c777141a08aefd6dab0d
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-01-17 03:46:45 +0530

    Time: 16 ms, Memory: 45.2 MB - LeetHub

--- 2002-1499-720-longest-word-in-dictionary/2002-1499-720-longest-word-in-dictionary.java
+++ 2002-1499-720-longest-word-in-dictionary/2002-1499-720-longest-word-in-dictionary.java
@@ -1,17 +1,17 @@
 class Solution {
     public String longestWord(String[] words) {
         Arrays.sort(words);
-        HashSet<String> set=new HashSet<>();
-        String ans="";
-        for(int i=0;i<words.length;i++){
-            String s=words[i];
-            if(s.length()==1 || set.contains(s.substring(0,s.length()-1))){
-                if(s.length()>ans.length()){
-                    ans=s;
+        HashSet<String> set = new HashSet<>();
+        String ans = "";
+        for (int i = 0; i < words.length; i++) {
+            String s = words[i];
+            if (s.length() == 1 || set.contains(s.substring(0, s.length() - 1))) {
+                if (s.length() > ans.length()) {
+                    ans = s;
                 }
-            set.add(s);    
+                set.add(s);
             }
-            
+
         }
         return ans;
     }
