4b6a6bd6e213ee2ed48bcd9678fe290683c7a22e
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-01-16 09:22:36 +0530

    Time: 0 ms, Memory: 40.4 MB - LeetHub

---
+++ 1022-71-2429-minimize-xor/1022-71-2429-minimize-xor.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isSetBit(int num,int bit){
+        return ((num & (1<<bit))!=0);
+    }
+
+    public boolean isUnSetBit(int num,int bit){
+        return ((num&(1<<bit))==0);
+    }
+
+    public int minimizeXor(int num1, int num2) {
+        int x=num1;
+        int num1SetBitCount=Integer.bitCount(num1);
+        int num2SetBitCount=Integer.bitCount(num2);
+        int bit=0;
+        if(num1SetBitCount<num2SetBitCount){
+            while(num1SetBitCount<num2SetBitCount){
+                if(!isSetBit(x,bit)){ //if bit is unset then make it set
+                    x=x|(1<<bit); //here we are setting the bit 
+                    num1SetBitCount++; //for count addition
+                }
+                else{
+                    bit++;
+                }
+            }
+        }
+
+        else if(num1SetBitCount>num2SetBitCount){
+            while(num1SetBitCount>num2SetBitCount){
+                if(isSetBit(x,bit)){ //checking the bit is set then make it unset
+                    x=x&(~(1<<bit));
+                    num1SetBitCount--; //decreasing the count to amke it equal
+                }
+                else{
+                    bit++;
+                }
+            }
+        }    
+        //if the bit count are equal then  you acn return  num1 which is x;
+        return x;
+        
+    }
+}
