608eb86e4e4909fd50f33dce259b758a50c2e488
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-01-21 05:58:54 +0530

    Time: 6 ms, Memory: 45.9 MB - LeetHub

--- 1175-2944-661-image-smoother/1175-2944-661-image-smoother.java
+++ 1175-2944-661-image-smoother/1175-2944-661-image-smoother.java
@@ -1,28 +1,88 @@
 class Solution {
     public int[][] imageSmoother(int[][] img) {
-        int row=img.length;
-        int col=img[0].length;
-        int [][] ans=new int[row][col];
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                //i will be ranging from i-1 to i+1
-                //j will be ranging from j-1 to j+1
+        int m =img.length;
+        int n=img[0].length;
+
+        int [] prevRow=new int[n];
+        int topCorner=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+
                 int sum=0;
                 int count=0;
-                for(int x=-1 ;x<=1;x++){
-                    for(int y=-1;y<=1;y++){
-                        int rIndex=i+x;
-                        int cIndex=j+y;
+                //Bottom Corner neighbour->[i+1][j-1],[i+1][j],[i+1][j+1]
+                if(i+1<m){
+                    if(j-1>=0){
+                       sum=sum+ img[i+1][j-1];
+                       count++;
+                    }
 
-                        if(rIndex>=0 && rIndex<row && cIndex>=0 && cIndex<col){
-                            sum=sum+img[rIndex][cIndex];
-                            count++;
-                        }
+                    sum=sum+img[i+1][j]; //this will not be out of bound j is in the range
+                    count++;
+
+                    if(j+1<n){
+                        sum=sum+img[i+1][j+1];
+                        count++;
                     }
                 }
-                ans[i][j]=sum/count;
+
+                // just right neighbour
+                //[i][j+1]
+
+                if(j+1<n){
+                    sum=sum+img[i][j+1];
+                    count++;
+                }
+
+
+            //just left
+            //[i][j-1]
+
+            if(j-1>=0){
+                sum=sum+prevRow[j-1];
+                count++;
+            }
+
+                //current cell
+
+                sum=sum+img[i][j];
+                count++;
+
+                //Top neighbours i-1th row
+
+                if(i-1>=0){
+                    //[i-1][j-1]
+                    if(j-1>=0){
+                        sum=sum+topCorner;
+                        count++;
+                    }
+
+                //[i-1][j] ,[i-1][j+1]
+
+                sum=sum+prevRow[j];
+                count++;
+
+                //[i-1][j+1]
+                if(j+1<n){
+                    sum=sum+prevRow[j+1];
+                    count++;
+                }
+            
+            }
+
+           
+
+            if(i-1>=0){
+            topCorner=prevRow[j];
+            }
+            prevRow[j]=img[i][j];
+
+            img[i][j]=sum/count;
+
             }
         }
-        return ans;
+
+        return img;
     }
 }
