de8f15c4782c3762c46f3cc127d97bdb9d1a01dd
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-02-05 00:54:21 +0530

    Time: 0 ms, Memory: 43.5 MB - LeetHub

---
+++ 0493-28-3151-special-array-i/0493-28-3151-special-array-i.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public boolean isArraySpecial(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(i+1<n && (nums[i] & 1)==0 && (nums[i+1]&1)==0){
+                return false;
+            }
+            else if(i+1<n && (nums[i]& 1)!=0 && (nums[i+1]&1)!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+   /* public boolean isArraySpecial(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(i+1<n && nums[i]%2==0 && nums[i+1]%2==0){
+                return false;
+            }
+            else if(i+1<n && nums[i]%2!=0 && nums[i+1]%2!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}*/
