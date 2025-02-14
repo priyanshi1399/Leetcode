5349e6de59905eaf7f33f132277bfb9308ead30b
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-02-14 05:58:46 +0530

    Time: 0 ms, Memory: 41.1 MB - LeetHub

--- 4975-355-119-pascals-triangle-ii/4975-355-119-pascals-triangle-ii.java
+++ 4975-355-119-pascals-triangle-ii/4975-355-119-pascals-triangle-ii.java
@@ -1,5 +1,22 @@
 class Solution {
+    public int nCr(int n,int r){
+        long ans=1;
+        for(int i=0;i<r;i++){
+            ans=ans*(n-i);
+            ans=ans/(i+1);
+        }
+        return (int)ans;
+    }
     public List<Integer> getRow(int rowIndex) {
+        List<Integer> res=new ArrayList<>();
+        for(int col=0;col<=rowIndex;col++){
+            res.add(nCr(rowIndex,col));
+        }
+        return res;
+
+    }
+}
+  /*  public List<Integer> getRow(int rowIndex) {
         List<Integer> ans= new ArrayList<>();
         ans.add(1);
         long res=1;
@@ -11,4 +28,4 @@ class Solution {
         return ans;
 
     }
-}
+}*/
