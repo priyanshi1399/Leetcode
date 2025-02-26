46fae7e699ea3b07d490d2767fb2668f17564c15
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-02-26 14:18:29 +0530

    Time: 8 ms, Memory: 60.1 MB - LeetHub

---
+++ 1946-93-1524-number-of-sub-arrays-with-odd-sum/1946-93-1524-number-of-sub-arrays-with-odd-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    private final int mod=1000000007;
+    public int numOfSubarrays(int[] arr) {
+        int n=arr.length;
+        int [] prefixSum=new int[n];
+        prefixSum[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefixSum[i]=prefixSum[i-1]+arr[i];
+        }
+        int evenArrays=1; //why 1 becausewe start from 0 anbd 0 is even
+        int oddArrays=0;
+        int ans=0;
+        for(int i=0;i<prefixSum.length;i++){
+            if(prefixSum[i]%2==0){
+                ans=(ans+oddArrays)%mod;
+                evenArrays++;
+            }
+            else{
+                ans=(ans+evenArrays)%mod;
+                oddArrays++;
+
+            }
+        }
+        return ans;
+    }
+}
