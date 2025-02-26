64cfdf328c1c376ab2826b22742367a1e9922e33
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-02-26 14:40:58 +0530

    Time: 7 ms, Memory: 56 MB - LeetHub

--- 1946-93-1524-number-of-sub-arrays-with-odd-sum/1946-93-1524-number-of-sub-arrays-with-odd-sum.java
+++ 1946-93-1524-number-of-sub-arrays-with-odd-sum/1946-93-1524-number-of-sub-arrays-with-odd-sum.java
@@ -1,6 +1,27 @@
 class Solution {
     private final int mod=1000000007;
-    public int numOfSubarrays(int[] arr) {
+     public int numOfSubarrays(int[] arr) {
+        int n=arr.length;
+        int sum=0;
+        int evenArrays=1; //why 1 becausewe start from 0 anbd 0 is even
+        int oddArrays=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+            if(sum%2==0){
+                ans=(ans+oddArrays)%mod;
+                evenArrays++;
+            }
+            else{
+                ans=(ans+evenArrays)%mod;
+                oddArrays++;
+
+            }
+        }
+        return ans;
+    }
+}
+    /*public int numOfSubarrays(int[] arr) {
         int n=arr.length;
         int [] prefixSum=new int[n];
         prefixSum[0]=arr[0];
@@ -23,4 +44,4 @@ class Solution {
         }
         return ans;
     }
-}
+}*/
