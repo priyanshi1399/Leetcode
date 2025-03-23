0754f44ce783536484d876caf6c328a57126b471
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-03-24 04:08:34 +0530

    Time: 7 ms, Memory: 45.1 MB - LeetHub

--- 13045-315-51-n-queens/13045-315-51-n-queens.java
+++ 13045-315-51-n-queens/13045-315-51-n-queens.java
@@ -1,60 +1,44 @@
 class Solution {
-    private boolean isSafe(int row,int col,List<String> board,int n){
-        //upward safe
-
-        for(int i=row;i>=0;i--){
-            if(board.get(i).charAt(col)=='Q'){
-                return false;
-            }
-        }
-
-        //for left diagonal check
-        for(int i=row, j=col;i>=0 && j>=0; i-- , j--){
-            if(board.get(i).charAt(j)=='Q'){
-                return false;
-            }
-        }
-
-        //for right diagonal
-        for(int i=row,j=col; i>=0 && j<n; i-- , j++){
-            if(board.get(i).charAt(j)=='Q'){
-                return false;
-            }
-        }
-        return true;
-
-    }
-    private void solve(int row,List<String> board,List<List<String>> result,int n){
-        if(row==n){
+        Set<Integer> diagonal=new HashSet<>();
+        Set<Integer> antiDiagonal=new HashSet<>();
+        Set<Integer> col=new HashSet<>();
+        List<List<String>> result=new ArrayList<>();
+    private void solve(int row, List<String> board,int n){
+        if(row>=n){
             result.add(new ArrayList<>(board));
             return;
         }
 
-        for(int col=0;col<n;col++){
-            if(isSafe(row,col,board,n)){
-                StringBuilder newRow=new StringBuilder(board.get(row)); //first row getting
-                newRow.setCharAt(col,'Q'); //set that col value with Q
-                board.set(row,newRow.toString()); //updating the board
-                solve(row+1,board,result,n);
-                newRow.setCharAt(col,'.');
-                board.set(row,newRow.toString()); //updating the board
+        for(int j=0;j<n;j++){
+            if(diagonal.contains(row+j) || antiDiagonal.contains(row-j) || col.contains(j)){
+                continue;
             }
+            diagonal.add(row+j);
+            antiDiagonal.add(row-j);
+            col.add(j);
+            StringBuilder temp=new StringBuilder(board.get(row));
+            temp.setCharAt(j,'Q');
+            board.set(row,temp.toString());
+            solve(row+1,board,n);
+            diagonal.remove(row+j);
+            antiDiagonal.remove(row-j);
+            col.remove(j);
+            temp.setCharAt(j,'.');
+            board.set(row,temp.toString());
+
         }
     }
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result=new ArrayList<>();
-        //making a board
         List<String> board=new ArrayList<>();
         for(int i=0;i<n;i++){
             StringBuilder curr=new StringBuilder();
             for(int j=0;j<n;j++){
                 curr.append('.');
             }
-            board.add(curr.toString()); //[. . . .][. . . .][. . . .][. . . .]
+            board.add(curr.toString());
         }
 
-        solve(0,board,result,n);
+        solve(0,board,n); //row is 0
         return result;
-
     }
 }
