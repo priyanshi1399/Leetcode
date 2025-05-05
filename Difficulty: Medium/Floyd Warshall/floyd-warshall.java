84d37a28d2f107aa0dfdd1e4d5879f1f5dac52a4
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-05-05 16:28:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial template for JAVA
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                String[] s = br.readLine().trim().split(" ");
+                for (int j = 0; j < n; j++) matrix[i][j] = Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.floydWarshall(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function template for JAVA
+
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        int n=dist.length;
+       
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][via]==100000000 || dist[via][j]==100000000){
+                        continue;
+                    }
+                    dist[i][j]=Math.min(dist[i][j], (dist[i][via]+dist[via][j]));
+                }
+            }
+        }
+      
+    }
+}
