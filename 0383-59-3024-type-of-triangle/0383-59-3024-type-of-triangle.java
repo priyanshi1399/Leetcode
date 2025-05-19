b4ac0c3b505d991c2ce02472e181bbc3ea1dc523
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-05-20 03:55:36 +0530

    Time: 0 ms, Memory: 42 MB - LeetHub

---
+++ 0383-59-3024-type-of-triangle/0383-59-3024-type-of-triangle.java
@@ -0,0 +1,26 @@
+class Solution {
+    private boolean valid(int [] nums){
+            if(nums[0]+nums[1]>nums[2] &&  nums[0]+nums[2]>nums[1] && nums[1]+nums[2]>nums[0]){
+                return true;
+            }
+            return false;
+        }
+    public String triangleType(int[] nums) {
+        
+        if(!valid(nums)){
+            return "none";
+        }
+        if(nums[0]==nums[1] && nums[1]==nums[2] && nums[0]==nums[2]){
+                return "equilateral";
+            }
+        else if(nums[0]!=nums[1] && nums[1]!=nums[2] && nums[0]!=nums[2]){
+                return "scalene";
+                
+            }
+        else{
+                return "isosceles";
+            }
+
+                
+    }
+}
