4fd84f93f7215d0519b8cc772281b82b86694057
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-02 01:55:02 +0530

    Time: 1 ms (94.31%) | Memory: 41.1 MB (31.75%) - LeetSync

--- 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.java
+++ 3199-distribute-candies-among-children-i/distribute-candies-among-children-i.java
@@ -3,11 +3,10 @@
     int count=0;
         for(int ch1=0;ch1<=limit;ch1++){
             for(int ch2=0;ch2<=limit;ch2++){
-                for(int ch3=0;ch3<=limit;ch3++){
-                    if(ch1+ch2+ch3==n){
-                        count++;
-                    }
-                }
+               int ch3=n-ch1-ch2;
+               if(ch3>=0 && ch3<=limit){
+                count++;
+               }
             }
         }
         return count;
