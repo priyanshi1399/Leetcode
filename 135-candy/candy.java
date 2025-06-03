53a06001185956a56e7a8cbc8b8f1e0ef345b48c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-03 18:02:49 +0530

    Time: 3 ms (85.01%) | Memory: 46.2 MB (24.18%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,31 +1,33 @@
 class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
-        int [] left=new int[n];
-        Arrays.fill(left,1);
-        for(int i=1;i<n;i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
+        int candy=n;
+
+        int i=1;
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                continue;
             }
-        }
-
-        int [] right=new int[n];
-        Arrays.fill(right,1);
-        for(int i=n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+            int peak=0;
+            while(ratings[i]>ratings[i-1]){
+                peak++;
+                candy+=peak;
+                i++;
+                if(i==n){
+                    return candy;
+                }
             }
-        }
+            int dip=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                dip++;
+                candy+=dip;
+                i++;
+            }
 
-        int [] ans=new int[n];
-        int sum=0;
-        for(int i=0;i<n;i++){
-            ans[i]=Math.max(left[i],right[i]);
-            sum+=ans[i];
+            candy-=Math.min(peak,dip);
         }
-
-        return sum;
-        
+        return candy;
 
     }
 }
