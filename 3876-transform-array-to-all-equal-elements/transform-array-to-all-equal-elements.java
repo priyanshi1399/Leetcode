b4d04bababf746dd487686486b28515215a4a490
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-09 15:32:39 +0530

    Time: 10 ms (100.00%) | Memory: 63 MB (100.00%) - LeetSync

---
+++ 3876-transform-array-to-all-equal-elements/transform-array-to-all-equal-elements.java
@@ -0,0 +1,32 @@
+class Solution {
+   int n;
+
+    public int makeEveryElement1(int [] temp,int k,int val){ 
+       int op = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (temp[i] != val) {
+                temp[i] *=-1;;
+                temp[i + 1] *=-1;
+                op++;
+                if (op > k) {
+                    return Integer.MAX_VALUE; 
+                }
+            }
+        }
+        if (temp[n - 1] != val) {
+            return Integer.MAX_VALUE;
+        }
+        return op;
+
+        
+    }
+    public boolean canMakeEqual(int[] nums, int k) {
+        n=nums.length;
+         int[] temp1 = nums.clone(); // copy for val = 1
+        int[] temp2 = nums.clone(); // copy for val = -1
+
+        return ((makeEveryElement1(temp1,k,1)<=k) || (makeEveryElement1(temp2,k,-1)<=k)); 
+
+
+    }
+}
