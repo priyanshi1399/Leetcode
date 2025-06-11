4e629358a18626be3e9b8eb07774e5d414fa8008
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-12 01:53:32 +0530

    Time: 6 ms (21.21%) | Memory: 44.6 MB (97.95%) - LeetSync

---
+++ 51-n-queens/n-queens.java
@@ -0,0 +1,61 @@
+class Solution {
+    List<List<String>> result=new ArrayList<>();
+    public boolean isSafe(int row,int col,List<String> board,int n){
+        //upward
+        for(int i=row;i>=0;i--){
+            if(board.get(i).charAt(col)=='Q'){
+                return false;
+            }
+        }
+
+        //upward left diagonal
+
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board.get(i).charAt(j)=='Q'){
+                return false;
+            }
+        }
+
+
+        //upward right diagonal
+        for(int i=row,j=col;i>=0 && j<n;i--,j++){
+            if(board.get(i).charAt(j)=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public void solve(int row,List<String> board,int n){
+        if(row==n){
+            result.add(new ArrayList<>(board));
+            return;
+        }
+
+
+        for(int col=0;col<n;col++){
+            if(isSafe(row,col,board,n)){
+                StringBuilder newRow=new StringBuilder(board.get(row)); //getting first row
+                newRow.setCharAt(col,'Q'); //setting Q at index 0
+                board.set(row,newRow.toString());//setting the value toString
+                solve(row+1,board,n);
+                newRow.setCharAt(col,'.'); //setting . because of backtracking
+                board.set(row,newRow.toString());//setting the value toString
+            }
+        }
+    }
+    public List<List<String>> solveNQueens(int n) {
+               
+        List<String> board=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            StringBuilder curr=new StringBuilder();
+            for(int j=0;j<n;j++){
+                curr.append('.');
+            }
+            board.add(curr.toString());
+        }
+
+        solve(0,board,n);
+        return result;
+        
+    }
+}
