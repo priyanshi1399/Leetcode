69ffaff71c32a55355dac1d18a036982f530df71
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-17 04:06:06 +0530

    Time: 17 ms (15.94%) | Memory: 44.7 MB (73.78%) - LeetSync

---
+++ 3893-generate-tag-for-video-caption/generate-tag-for-video-caption.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String generateTag(String caption) {
+        String [] parts=caption.split(" ");
+        StringBuilder str=new StringBuilder("#");
+        for(int i=0;i<parts.length;i++){ 
+            if (parts[i].equals(" ") || parts[i].equals("")) {
+                continue;
+            }
+            else{
+                System.out.println(parts[i]);
+                if(str.length()==1){
+                    str.append(String.valueOf(parts[i].charAt(0)).toLowerCase());
+                }
+                else{
+                str.append(String.valueOf(parts[i].charAt(0)).toUpperCase());
+                }
+                int len=parts[i].length();
+                String sub=parts[i].substring(1,len).toLowerCase();
+                str.append(sub);
+            }
+        }
+
+        String res=str.toString();
+        if(res.length()<100){
+            return res;
+        }
+        return str.substring(0,100);
+    }
+}
