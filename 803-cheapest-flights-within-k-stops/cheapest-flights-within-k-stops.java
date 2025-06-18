d6eb00b6402b3f4b24a857bb7dec86be5bec757d
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-18 19:45:25 +0530

    Time: 6 ms (83.02%) | Memory: 44.7 MB (27.03%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+
+        List<List<int []>> adjList=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjList.add(new ArrayList<>());
+        }
+        for(int [] f: flights){
+            int u=f[0];
+            int v=f[1];
+            int w=f[2];
+
+            adjList.get(u).add(new int []{v,w});
+        }
+        int [] res=new int[n];
+        Arrays.fill(res,Integer.MAX_VALUE);
+        res[src]=0;
+        Queue<int[]> q=new LinkedList<>();
+
+        q.add(new int [] {src,0});
+        int level=0;
+        while(!q.isEmpty() && level<=k){
+            int size=q.size();
+            while(size-->0){
+                int [] curr=q.poll();
+                int srcNode=curr[0];
+                int c=curr[1];
+                
+                for(int [] neighbor: adjList.get(srcNode)){
+                    int adjNode=neighbor[0];
+                    int cost=neighbor[1];
+
+                    if(c+cost<res[adjNode]){
+                        res[adjNode]=c+cost;
+                        q.add(new int []{adjNode,c+cost});
+                    }
+                }
+            }
+                level++;
+            
+        }
+        if(res[dst]!=Integer.MAX_VALUE){
+            return res[dst];
+        }
+        return -1;
+    }
+}
