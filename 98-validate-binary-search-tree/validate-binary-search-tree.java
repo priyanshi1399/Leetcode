ba6fe11ce6da7936d0bce545e337204727187eee
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-24 23:43:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (45.23%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,24 +14,16 @@
  * }
  */
 class Solution {
-    public boolean solve(TreeNode root,long minValue, long maxValue){
+    public boolean solve(TreeNode root, long minValue,long maxValue){
         if(root==null){
             return true;
         }
         if(root.val>minValue && root.val<maxValue){
             return (solve(root.left,minValue,root.val) && solve(root.right,root.val,maxValue));
-
         }
-        else{
-            return false;
-        }
-        
+        return false;
     }
     public boolean isValidBST(TreeNode root) {
-        if(root==null){
-            return true;
-        }
-
         return solve(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
 }
