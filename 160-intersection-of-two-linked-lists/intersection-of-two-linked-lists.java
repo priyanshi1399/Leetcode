d85446854ad386e958023491fa95d3443fc7354d
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-27 02:08:00 +0530

    Time: 1 ms (99.86%) | Memory: 48.3 MB (83.44%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,20 +11,65 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode,Integer> map=new HashMap<>();
-        ListNode curr1=headA;
+        int size1=0;
+        int size2=0;
+        ListNode curr1=headA;  
         while(curr1!=null){
-            map.put(curr1,1);
+            size1++;
             curr1=curr1.next;
         }
-
         ListNode curr2=headB;
-        while(curr2!=null){
-            if(map.containsKey(curr2)){
-                return curr2;
-            }
+         while(curr2!=null){
+            size2++;
             curr2=curr2.next;
         }
-        return null;
+        curr1=headA;
+        curr2=headB;
+        int nodeToSkip=Math.abs(size1-size2);
+        if(size1>size2){
+            
+            for(int i=1;i<=nodeToSkip;i++){
+                curr1=curr1.next; //same level
+            }
+            
+            while(curr1!=curr2){
+                curr1=curr1.next;
+                curr2=curr2.next;
+            }
+            if(curr1!=null && curr2!=null){
+                return curr1;
+            }
+            return null;
+        }
+
+        else if(size1==size2){
+             while(curr1!=curr2){
+                curr1=curr1.next;
+                curr2=curr2.next;
+            }
+            if(curr1!=null && curr2!=null){
+                return curr1;
+            }
+            return null;
+
+        }
+        else{
+           
+            for(int i=1;i<=nodeToSkip;i++){
+                curr2=curr2.next; //same level
+            }
+            
+            while(curr1!=curr2){
+                curr1=curr1.next;
+                curr2=curr2.next;
+            }
+            if(curr1!=null && curr2!=null){
+                return curr1;
+            }
+            return null;
+        }
+
+
+
     }
 }
