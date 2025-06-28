e4803d4cf23f10d54d81cb6627558068b1ec8155
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-06-28 10:51:30 +0530

    Time: 6 ms (22.07%) | Memory: 48.5 MB (76.45%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,38 +10,22 @@
  * }
  */
 public class Solution {
-    public ListNode collission(ListNode curr1,ListNode curr2,int nodeToSkip){
-        for(int i=1;i<=nodeToSkip;i++){
-                curr1=curr1.next; //same level
-            }
-        while(curr1!=curr2){
-                curr1=curr1.next;
-                curr2=curr2.next;
-            }
-            return curr1;
-    }
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        int size1=0;
-        int size2=0;
-        ListNode curr1=headA;  
-        while(curr1!=null){
-            size1++;
-            curr1=curr1.next;
+        HashMap<ListNode,Integer> map=new HashMap<>();
+        ListNode curr=headA;
+        while(curr!=null){
+           map.put(curr,1);
+           curr=curr.next;
         }
-        ListNode curr2=headB;
-         while(curr2!=null){
-            size2++;
-            curr2=curr2.next;
-        }
-        curr1=headA;
-        curr2=headB;
-        int nodeToSkip=Math.abs(size1-size2);
-        if(size1>size2){
-            return collission(curr1,curr2,nodeToSkip);
-        }
-       
-        return collission(curr2,curr1,nodeToSkip);
-        
 
+        curr=headB;
+        while(curr!=null){
+            if(map.containsKey(curr)){
+                return curr;
+            }
+            curr=curr.next;
+        }
+
+        return null;
     }
 }
