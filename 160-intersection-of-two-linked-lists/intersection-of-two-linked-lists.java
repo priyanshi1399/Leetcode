9f4a0947d5ba90143b2ce7ab231be51f0f99e82d
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-02 00:48:03 +0530

    Time: 1 ms (99.87%) | Memory: 48.6 MB (56.34%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -10,21 +10,39 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode,Integer> map=new HashMap<>();
-        ListNode curr1=headA;
-        while(curr1!=null){
-            map.put(curr1,1);
+    public  ListNode collission(ListNode curr1,ListNode curr2, int diff){
+        for(int i=1;i<=diff;i++){
             curr1=curr1.next;
-        } 
+        }
 
-        ListNode curr2=headB;
-        while(curr2!=null){
-            if(map.containsKey(curr2)){
-                return curr2; 
-            }
+        while(curr1!=curr2){
+            curr1=curr1.next;
             curr2=curr2.next;
         }
-        return null;
+        return curr1; //either return curr1 or curr2;
+    }
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curr=headA;
+        int size1=0;
+        while(curr!=null){
+            size1++;
+            curr=curr.next;
+        }
+
+        curr=headB;
+        int size2=0;
+        while(curr!=null){
+            size2++;
+            curr=curr.next;
+        }
+
+        ListNode curr1=headA;
+        ListNode curr2=headB;
+        int diff=Math.abs(size1-size2);
+        if(size1>=size2){
+            return collission(curr1,curr2,diff);
+        }
+        return  collission(curr2,curr1,diff);
+       
     }
 }
