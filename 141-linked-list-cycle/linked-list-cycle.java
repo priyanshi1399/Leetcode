387a460f9114a6eecdd859374101c61638565a53
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-01 18:57:47 +0530

    Time: 5 ms (7.84%) | Memory: 44.8 MB (27.79%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -14,13 +14,12 @@ public class Solution {
         HashSet<ListNode> seen=new HashSet<>();
         ListNode curr=head;
         while(curr!=null){
-            if(seen.contains(curr)){
+            if(seen.contains(curr)){ //if it has already that node
                 return true;
             }
             seen.add(curr);
             curr=curr.next;
         }
         return false;
-
     }
 }
