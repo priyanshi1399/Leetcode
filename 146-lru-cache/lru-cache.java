12eb4f40087a487b3914bc76d18ee25c1edf3ea9
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-08 00:38:35 +0530

    Time: 1057 ms (5.01%) | Memory: 115.2 MB (43.13%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,95 +1,42 @@
-class Node{
-    Node next,prev;
-    int key,val;
-
-    Node(){
-        key=val=-1; //to avoid null pointer  act like dummy
-        next=prev=null;
-    }
-    Node(int key,int val){
-        this.key=key;
-        this.val=val;
-        prev=next=null;
-    }
-}
 class LRUCache {
-    private HashMap<Integer,Node> map;
-    private int cap;
-    private Node head;
-    private Node tail;
+    HashMap<Integer,Integer> map;
+    Queue<Integer> q;
+    int cap;
 
-    private void InsertAfterHead(Node node){
-        Node nextNode=head.next;
-        node.next=nextNode;
-        nextNode.prev=node;
-        head.next=node;
-        node.prev=head;
-
-    }
-    private void deleteNode(Node node){
-        Node prevNode=node.prev; //node before  Node prev 
-        Node nextNode=node.next; //next node after node
-
-        //connecting both after deleting
-        nextNode.prev=prevNode;
-        prevNode.next=nextNode;
-
-    }
     public LRUCache(int capacity) {
-        map=new HashMap<>();
         cap=capacity;
-        head=new Node();
-        tail=new Node();
-
-        //link make the connection
-        tail.prev=head;
-        head.next=tail;
+        map=new HashMap<>();
+        q=new LinkedList<>();
     }
     
     public int get(int key) {
-        if(!map.containsKey(key)){
-            return -1;
+        if(map.containsKey(key)){
+            int val=map.get(key);
+            q.remove(key); //first remove
+            q.add(key); //push because just now/recently used 
+            return val;
         }
-
-        Node node=map.get(key); //getting node
-        int value=node.val; //getting the value of the node
-
-        deleteNode(node); //because now it will come at the place of most used after head
-
-        InsertAfterHead(node); //insert after head
-        return value;
+        return -1;
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-            Node node=map.get(key); //get the node
-            node.val=value; //update with new value
+            int val=map.get(key);
+            q.remove(key); //removing this key from queue
 
-            deleteNode(node); //delete beacuse it will come most frequently used not recently
-
-            InsertAfterHead(node); //insert after the head
-            map.put(key,node); //insert updated value to map
+            map.put(key,value); //putting new value
+            q.add(key); //became recently used add again
             return;
 
         }
-
-        if(map.size()>=cap){
-
-            Node node=tail.prev; //get node from prev
-
-            int val=node.key; //get the val means to remove the key from the map
-
-            map.remove(val); //remove from the map
-
-            deleteNode(node); //delete beacuse its not there
+        else if(map.size()>=cap && !q.isEmpty()){
+            int keyToRemove=q.poll(); //get the key from queue and remove from map
+            map.remove(keyToRemove);
 
         }
+        map.put(key,value);
+        q.add(key);
 
-        Node newNode=new Node(key,value); //create new Node
- 
-        map.put(key,newNode); //put into map
-
-        InsertAfterHead(newNode); //Insert at the most used.
     }
 }
 
