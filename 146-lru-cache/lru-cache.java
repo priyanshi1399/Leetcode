b8c8af9dd66bfd57013663dde104cbdb0b36d7e8
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-07 23:24:03 +0530

    Time: 42 ms (93.07%) | Memory: 114 MB (59.64%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,33 +1,100 @@
+class ListNode{
+    public ListNode next,prev;
+    public int key,  val;
+
+    ListNode(){
+        key=val=-1;
+        next=prev=null;
+    }
+    ListNode (int key, int val){
+        this.key=key;
+        this.val=val;
+        prev=next=null;
+    }
+}
 class LRUCache {
-    HashMap<Integer,Integer> map;
-    Queue<Integer> queue;
-    int n;
+    private ListNode head;
+    private ListNode tail;
+    private HashMap<Integer,ListNode> map;
+    private  int cap;
+    
+    private void InsertAfterHead(ListNode node){
+        ListNode nextNode = head.next;
+        head.next = node;
+        nextNode.prev = node;
+        node.prev = head;
+        node.next = nextNode;
+        
+    }
+
+    
+
+    private void deleteNode(ListNode node){
+        ListNode nextNode=node.next;
+        ListNode prevNode=node.prev;
+
+        nextNode.prev=prevNode;
+        prevNode.next=nextNode;
+    }
+
     public LRUCache(int capacity) {
+        head=new ListNode();
+        tail=new ListNode();
         map=new HashMap<>();
-        queue=new LinkedList<>();
-        n=capacity;
+        cap=capacity;
+
+        //make the connection
+        head.next=tail;
+        tail.prev=head;
+
     }
     
     public int get(int key) {
-        if(map.containsKey(key)){
-            int val=map.get(key);
-            queue.remove(key);//remove also 
-            queue.add(key); //add because now it is used
-            return val;
+        if(!map.containsKey(key)){
+            return -1;
         }
-        return -1;
+
+        ListNode node=map.get(key); //getting the address so that we can get the data
+        int value=node.val;
+        //before getting value we have to delete that node from tail ans insert at head next beacuse it is recently used
+        deleteNode(node);
+       
+        //became recently used
+        InsertAfterHead(node);
+        return value;
+
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-            queue.remove(key);
+            ListNode node=map.get(key); //get the node 
+            node.val=value; //updating the value
+
+            //we have to delete from DLL
+            deleteNode(node);
+            //insert after head also because it is  most recently used
+
+            InsertAfterHead(node);
+
+            map.put(key,node);
+            return;
+           
         }
-        else if((map.size()>=n) && (!queue.isEmpty())){ //if both if will be there else if should be there
-            int keyToRemove=queue.poll();
-            map.remove(keyToRemove);
+
+        if(map.size()>=cap){
+            ListNode node=tail.prev; //get the node which is least recently used
+            map.remove(node.key);
+
+            deleteNode(node); //delete the node
+
         }
-        map.put(key,value);
-        queue.add(key);
+
+        ListNode newNode=new ListNode(key,value);
+
+        map.put(key,newNode);
+
+        InsertAfterHead(newNode);
+        
     }
 }
 
