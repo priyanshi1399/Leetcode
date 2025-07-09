fe020dac7bdd9356083464d214fe9dedb4ae1e69
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-09 16:08:46 +0530

    Time: 60 ms (81.47%) | Memory: 57.2 MB (80.03%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -2,7 +2,8 @@ class Solution {
     public int largestRectangleArea(int[] heights) {
         Stack<Integer> stck=new Stack<>();
         int n=heights.length;
-        int maxArea=Integer.MIN_VALUE;
+        int nse,pse;
+        int maxArea=0;
         for(int i=0;i<n;i++){
             while(!stck.isEmpty() && heights[stck.peek()]>=heights[i]){
                 //when you kick out the elemen you have found next smaller to it and if stck is not empty the its prev will be that element
@@ -10,8 +11,8 @@ class Solution {
 
                 //write the nse and pse for that
 
-                int nse=i;
-                int pse=stck.isEmpty()?-1:stck.peek();
+                nse=i;
+                pse=stck.isEmpty()?-1:stck.peek();
                 //index is that element
                 maxArea=Math.max(maxArea,heights[index]*(nse-pse-1));
             }
@@ -23,8 +24,8 @@ class Solution {
         while(!stck.isEmpty()){
             int index=stck.pop();
 
-            int nse=n; //no next smaller
-            int pse=stck.isEmpty()?-1:stck.peek();
+            nse=n; //no next smaller
+            pse=stck.isEmpty()?-1:stck.peek();
             maxArea=Math.max(maxArea,heights[index]*(nse-pse-1));
         }
         return maxArea;
