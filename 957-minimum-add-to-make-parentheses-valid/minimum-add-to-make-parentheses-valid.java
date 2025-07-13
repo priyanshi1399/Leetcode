1c117a3007240f2fee786597f1fb26e2acc2cd89
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-13 15:24:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (64.78%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,24 +1,19 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        int n=s.length();
+        int open=0;
         int close=0;
-        Stack<Character> stck=new Stack<>();
-       int i=0;
-       while(i<n){
+        for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             if(ch=='('){
-                stck.push(ch); 
+                close++;
+            }
+            else if(close>0 && ch==')'){
+                close--;
             }
             else{
-                if(ch==')' && !stck.isEmpty()){
-                    stck.pop(); //matched open nd close brackets
-                }
-                else if(ch==')' && stck.isEmpty()){
-                    close++; //you can't find a match
-                } 
+                open++;
             }
-            i++;
         }
-        return close+stck.size(); //left unmatched brackets+earlier closed ones left out
+        return open+close;
     }
 }
