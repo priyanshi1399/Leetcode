0e88b79606ec89fc9fc77366ffbfdc2f7d9b26ce
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-13 22:32:20 +0530

    Time: 158 ms (5.37%) | Memory: 45.5 MB (5.10%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValid(String curr){
+        int sum=0;
+        for(int i=0;i<curr.length();i++){
+            if(curr.charAt(i)=='('){
+                sum++;
+            }
+            else{
+                sum--;
+            }
+        if(sum<0){
+            return false;
+        }
+        }
+        return sum==0;
+    }
+    public void solve(String curr, List<String> res, int n){
+
+        if(curr.length()==2*n){
+            System.out.println(curr);
+            if(isValid(curr)){
+            res.add(curr.toString());
+            
+            }
+            return;
+            
+        }
+
+        curr=curr+"(";
+        solve(curr, res,n);
+        curr=curr.substring(0,curr.length()-1);
+
+        curr=curr+")";
+        solve(curr, res,n);
+         curr=curr.substring(0,curr.length()-1);
+      
+    }
+    public List<String> generateParenthesis(int n) {
+        String curr="";
+
+        List<String> res=new ArrayList<>();
+        solve(curr,res,n);
+        return res;
+    }
+}
