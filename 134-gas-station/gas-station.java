cbedfd4c45e7e6d3c79766b795ece3b4349a80bf
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-18 00:33:49 +0530

    Time: 2 ms (97.00%) | Memory: 56.5 MB (22.19%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -8,17 +8,17 @@ class Solution {
             costValue+=cost[i];
         }
         if(gasValue<costValue){
-            return -1;
+            return -1; //if gas is less how we will move?
         }
         int start=0; gasValue=0;
         for(int i=0;i<n;i++){
             gasValue=gasValue+gas[i]-cost[i];
 
             if(gasValue<0){
-                start=i+1;
+                start=i+1; //go to next Index
                 gasValue=0;
             }
         }
-        return gasValue>=0?start:-1;
+        return gasValue>=0?start:-1; //positive value we will get index
     }
 }
