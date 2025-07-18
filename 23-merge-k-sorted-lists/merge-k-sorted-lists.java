908664d677fe0dc2eb475085b5c096c26ce44d3a
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-19 03:01:51 +0530

    Time: 4 ms (78.41%) | Memory: 44.7 MB (28.63%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,42 +9,30 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoSortedList(ListNode l1, ListNode l2){
-        if(l1==null){
-            return l2;
-        }
-        if(l2==null){
-            return l1;
-        }
-
-        if(l1.val<=l2.val){
-        //l1 and l2 are the two list l1-->1--1--2 l2--2-->3 
-        l1.next=mergeTwoSortedList(l1.next,l2);
-        return l1; //these are already sorted
-        }
-        else{
-        l2.next=mergeTwoSortedList(l2.next,l1);
-        return l2; //these are already sorted
-        }
-       // return null;
-    }
-    public ListNode partitionAndMerge(int start,int end,ListNode [] lists){
-        if(start==end){ //one node
-            return lists[start];
-
-        }
-
-        int mid=start+(end-start)/2;
-        ListNode L1=partitionAndMerge(start,mid,lists);
-        ListNode L2=partitionAndMerge(mid+1,end,lists);
-        return mergeTwoSortedList(L1,L2);
-
-    }
     public ListNode mergeKLists(ListNode[] lists) {
-        int k=lists.length;
-        if(k==0){
+        
+        if(lists==null || lists.length==0){
             return null;
         }
-        return partitionAndMerge(0,k-1,lists);
+        PriorityQueue<ListNode> pq=new PriorityQueue<>(lists.length,(a,b)->a.val-b.val); //min heap
+        ListNode res=new ListNode(0);
+        ListNode tail=res;
+
+        for(ListNode node:lists){
+            if(node!=null){
+                pq.add(node);
+            }
+        }
+
+        while(!pq.isEmpty()){
+            tail.next=pq.poll();
+            tail=tail.next;
+
+            if(tail.next!=null){
+                pq.add(tail.next);
+            }
+        }
+        return res.next;
+
     }
 }
