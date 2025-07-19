1c8aa040cfddb700d08896cf9848fc648d000cb9
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-20 03:24:28 +0530

    Time: 13 ms (76.82%) | Memory: 49.1 MB (25.36%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,55 +1,21 @@
 class Solution {
-    class Pair <K,V>{
-
-        private final K key;
-        private final V value;
-
-        public Pair( K key,  V value){
-            this.key=key;
-            this.value=value;
-        }
-
-
-        public K getKey(){
-            return key;
-        }
-        public V getValue(){
-            return value;
-        }
-
-    }
     public int[] topKFrequent(int[] nums, int k) {
         int n=nums.length;
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-
-        PriorityQueue<Pair<Integer,Integer>> pq=new PriorityQueue<>((a,b)->Integer.compare(a.getValue(),b.getValue()));
-
-        
-
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare(b[1],a[1]));
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-             pq.add(new Pair<>(entry.getKey(),entry.getValue()));
-            while(pq.size()>k){
-                pq.poll();
-            }
-           
+            pq.add(new int[]{entry.getKey(),entry.getValue()}); //it will be having the highest value at the top
         }
-
-         for (Pair<Integer, Integer> pair : pq) {
-            System.out.println(pair.getKey()+ ":" +pair.getValue());
-            }
-        int idx=0;
-        int [] ans=new int[k];
-        while(!pq.isEmpty()){
-            ans[idx]=pq.poll().getKey();
-            idx++;
+        int index=0;
+        int [] res=new int[k];
+        while(k-->0){
+            int [] curr=pq.poll();
+            res[index]=curr[0];
+            index++;
         }
-            
-        return ans;
-
-
-        
+        return res;
     }
 }
