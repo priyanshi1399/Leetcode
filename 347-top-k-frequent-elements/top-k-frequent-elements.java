ed2e5203326b5445e9bedddeeff4865d00a3a7bb
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-20 15:10:37 +0530

    Time: 10 ms (96.03%) | Memory: 48.4 MB (77.03%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,39 +1,39 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-       int n=nums.length;
+        int n=nums.length;
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<n;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
+        //there can be max of elements will be nums.length any number can come maximum of length times
 
-        List<Integer>[] bucket=new List[nums.length+1];
+        List<Integer>[] bucket=new List[n+1]; //max of nums.length elements
+        //inside this Listof Arrays we will have arrayList bcz there can be maore element which can have same frequency
         for(int i=0;i<n+1;i++){
             bucket[i]=new ArrayList<>();
         }
 
+        //preparing one bucket with freq as the index and value as the key
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            int key=entry.getKey();
             int freq=entry.getValue();
+            int number=entry.getKey();
 
-            bucket[freq].add(key);
+            bucket[freq].add(number); //like 1 ,2 --> 2 times then at index 2 {1,2}
+
         }
 
-        //teher can be more than 2 elements whose frequency can be same or maximum 
-        //example 1,2-->2 times {1,2} on 2
-        List<Integer> ans=new ArrayList<>();
-        for(int i=bucket.length-1;i>=0 && ans.size()<k;i--){ //why result.size<k those many elements will come
-            if(bucket[i]!=null){
-                ans.addAll(bucket[i]);
+        List<Integer> res=new ArrayList<>();
+        for(int i=n ;i>=0 && res.size()<k;i--){ //it will fill k elements from last we can take inner loop also
+            if(!bucket[i].isEmpty()){
+                res.addAll(bucket[i]);
             }
         }
 
-        int[] result = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            result[i] = ans.get(i);
+        int [] answer=new int[k];
+        for(int i=0;i<k;i++){
+            answer[i]=res.get(i);
         }
-        return result;
-
-
-        
+        return answer;
+    
     }
 }
