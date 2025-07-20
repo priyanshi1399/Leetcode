838f73ae460c5851c6a1bcd17917d681745a5743
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-20 14:45:19 +0530

    Time: 11 ms (91.85%) | Memory: 48.4 MB (73.29%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,21 +1,39 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n=nums.length;
+       int n=nums.length;
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<n;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare(b[1],a[1]));
+
+        List<Integer>[] bucket=new List[nums.length+1];
+        for(int i=0;i<n+1;i++){
+            bucket[i]=new ArrayList<>();
+        }
+
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            pq.add(new int[]{entry.getKey(),entry.getValue()}); //it will be having the highest value at the top
+            int key=entry.getKey();
+            int freq=entry.getValue();
+
+            bucket[freq].add(key);
         }
-        int index=0;
-        int [] res=new int[k];
-        while(k-->0){
-            int [] curr=pq.poll();
-            res[index]=curr[0];
-            index++;
+
+        //teher can be more than 2 elements whose frequency can be same or maximum 
+        //example 1,2-->2 times {1,2} on 2
+        List<Integer> ans=new ArrayList<>();
+        for(int i=bucket.length-1;i>=0 && ans.size()<k;i--){ //why result.size<k those many elements will come
+            if(bucket[i]!=null){
+                ans.addAll(bucket[i]);
+            }
         }
-        return res;
+
+        int[] result = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            result[i] = ans.get(i);
+        }
+        return result;
+
+
+        
     }
 }
