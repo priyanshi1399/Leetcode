72fa64ed7eda1a4620b2367fbb352a5b74072148
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-23 11:01:21 +0530

    Time: 82 ms (57.30%) | Memory: 59.1 MB (16.90%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,30 +1,34 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int n=position.length;
-        if(n<2){
-            return 1;
-        }
-        int [][] pair=new int[n][2];
-        for(int i=0;i<n;i++){
-            pair[i][0]=position[i];
-            pair[i][1]=speed[i];
-        }
-
-        Arrays.sort(pair,(a,b)->Integer.compare(a[0],b[0]));
-
-        Stack<Double> stck=new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            double t=(target-pair[i][0])/(1.0*pair[i][1]);
-            if(stck.isEmpty()){
-                stck.push(t);
-
-            }
-            else{
-                if(t>stck.peek()){
-                    stck.push(t);
-                }
-            }
-        }
-        return stck.size();
+    int n=position.length;
+    if(n<2){
+        return 1; //only one fleet will be there
     }
+    int [][] pair=new int[n][2];
+    Stack<Double> stck=new Stack<>();
+     for(int i=0;i<n;i++){
+        pair[i][0]=position[i];
+        pair[i][1]=speed[i];
+    }
+
+    Arrays.sort(pair,(a,b)->Integer.compare(a[0],b[0]));
+
+    for(int i=n-1;i>=0;i--){
+        int pos=pair[i][0];
+        int sp=pair[i][1];
+        double t=1.0*(target-pos)/sp; // time=distance/spped
+        if(stck.isEmpty()){
+            stck.push(t); //1 fleet
+        }
+
+        
+        else{
+            if(t>stck.peek()){ //it can't catch up
+                stck.push(t);
+            }
+        }
+    }
+    return stck.size();
+
+}
 }
