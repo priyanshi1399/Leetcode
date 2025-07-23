944d642c5605fbbbc06dfc23dda44375758a455b
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-23 11:09:49 +0530

    Time: 76 ms (69.27%) | Memory: 58 MB (38.57%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -5,30 +5,24 @@ class Solution {
         return 1; //only one fleet will be there
     }
     int [][] pair=new int[n][2];
-    Stack<Double> stck=new Stack<>();
      for(int i=0;i<n;i++){
         pair[i][0]=position[i];
         pair[i][1]=speed[i];
     }
 
     Arrays.sort(pair,(a,b)->Integer.compare(a[0],b[0]));
-
-    for(int i=n-1;i>=0;i--){
-        int pos=pair[i][0];
-        int sp=pair[i][1];
-        double t=1.0*(target-pos)/sp; // time=distance/spped
-        if(stck.isEmpty()){
-            stck.push(t); //1 fleet
-        }
-
+    int carFleet=1;
+    double time=(target-pair[n-1][0])/(1.0*pair[n-1][1]);
+    for(int i=n-2;i>=0;i--){
+    double t=(target-pair[i][0])/(1.0*pair[i][1]);
+      if(t>time){
+        time=t;
+        carFleet++;
         
-        else{
-            if(t>stck.peek()){ //it can't catch up
-                stck.push(t);
-            }
-        }
+      }
+
     }
-    return stck.size();
+    return carFleet;
 
 }
 }
