43865f63b514931f69c0dae21d42d149412ccac3
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-23 23:51:00 +0530

    Time: 45 ms (68.78%) | Memory: 113.9 MB (59.29%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,36 +1,87 @@
+class ListNode{
+    public int key,value;
+    public ListNode prev,next;
+
+       public ListNode(){
+        value=key=-1;
+        next=prev=null;
+    }
+    public ListNode(int key,int value){
+        this.key=key;
+        this.value=value;
+        prev=next=null;
+    }
+}
 class LRUCache {
     int cap;
-    Queue<Integer> q;
-    HashMap<Integer,Integer> map;
+    ListNode head;
+    ListNode tail;
+    HashMap<Integer,ListNode> map;
+    
+
+
+    public void deleteNode(ListNode node){
+        ListNode nextNode=node.next;
+        ListNode prevNode=node.prev;
+
+        nextNode.prev=prevNode;
+        prevNode.next=nextNode;
+    }
+
+        public void insertAfterHead(ListNode node){
+        ListNode nextNode=head.next;
+        head.next=node;
+        node.prev=head;
+        nextNode.prev=node;
+        node.next=nextNode;
+    }
     public LRUCache(int capacity) {
         cap=capacity;
-        q=new LinkedList<>();
+        head=new ListNode();
+        tail=new ListNode();
         map=new HashMap<>();
+
+        head.next=tail;
+        tail.prev=head; //making the connection
+
     }
     
     public int get(int key) {
-        if(map.containsKey(key)){
-            int value=map.get(key);
-            q.remove(key);
-            q.add(key);
-            return value;
+        if(!map.containsKey(key)){
+            return -1;
         }
-        return -1;
+        ListNode node=map.get(key);
+        int val=node.value;
+        deleteNode(node);
+        insertAfterHead(node);
+        return val;
+
     }
     
     public void put(int key, int value) {
         if(map.containsKey(key)){
-            q.remove(key);
-            map.put(key,value);
-            q.add(key);
+            ListNode node=map.get(key);
+            node.value=value;
+            deleteNode(node);
+            map.put(key,node);
+            insertAfterHead(node);   
             return;
         }
         else if(map.size()>=cap){
-            int keyToRemove=q.poll();
-            map.remove(keyToRemove);
+           ListNode node= tail.prev;
+           int keyToRemove=node.key;
+           map.remove(keyToRemove);
+           deleteNode(node);
+           ListNode newNode=new ListNode(key,value);
+           map.put(key,newNode);
+           insertAfterHead(newNode);
+           return;
         }
-        map.put(key,value);
-        q.add(key);
+        else{
+           ListNode newNode=new ListNode(key,value);
+           map.put(key,newNode);
+           insertAfterHead(newNode);
+        }
     }
 }
 
