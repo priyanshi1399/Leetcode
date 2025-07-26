7442998cb5e4b45c08ee2f4665f4ee9423c82770
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-26 16:58:11 +0530

    Time: 2 ms (92.31%) | Memory: 41.5 MB (57.08%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,5 +1,12 @@
 class Solution {
-    public String countAndAppend(String ans){
+   
+
+    public String countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+
+        String ans=countAndSay(n-1);
         StringBuilder str=new StringBuilder();
         for(int i=0;i<ans.length();i++){
             int count=1;
@@ -15,14 +22,5 @@ class Solution {
         
         return str.toString();
 
-    }
-
-    public String countAndSay(int n) {
-        if(n==1){
-            return "1";
-        }
-
-        String ans=countAndSay(n-1);
-        return countAndAppend(ans);
     }
 }
