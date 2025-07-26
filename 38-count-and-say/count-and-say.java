a5edd118a290124bd1bc9eea72ca3d52b753db61
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-26 19:25:04 +0530

    Time: 7 ms (46.03%) | Memory: 42.1 MB (28.07%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,26 +1,23 @@
 class Solution {
-   
-
     public String countAndSay(int n) {
-        if(n==1){
-            return "1";
-        }
-
-        String ans=countAndSay(n-1);
-        StringBuilder str=new StringBuilder();
-        for(int i=0;i<ans.length();i++){
-            int count=1;
-            char ch=ans.charAt(i);
-            while(i+1<ans.length() && ans.charAt(i)==ans.charAt(i+1)){
-                count++;
-                i++;
+        String res="1";
+        int count=1;
+        for(int i=1;i<n;i++){
+            count=1;
+            StringBuilder str=new StringBuilder();
+            for(int j=1;j<res.length();j++){
+                if(res.charAt(j)==res.charAt(j-1)){
+                    count++;
+                }
+                else{
+                    str.append(count).append(res.charAt(j-1)); //adding into the stringbUilder when not equal
+                    count=1;
+                }
             }
-            str.append(count);
-            str.append(ch);
-
+            str.append(count).append(res.charAt(res.length()-1));
+            res=str.toString();
+           // System.out.println(res);
         }
-        
-        return str.toString();
-
+        return res.toString();
     }
 }
