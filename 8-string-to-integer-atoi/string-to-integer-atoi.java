a82b21df5cffcbf19769b18f39a3e1e9737706d4
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-27 01:09:48 +0530

    Time: 1 ms (100.00%) | Memory: 42.3 MB (80.38%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -3,32 +3,29 @@ class Solution {
         int n=s.length();
         double sum=0;
         int i=0;
+        int sign=1;
+
         while(i<n  && s.charAt(i)==' '){
-                i++;
+                i++; //white spaces
         }
 
-        int sign=1;
-        if(i<n && s.charAt(i)=='+'){
-                sign=1;
-                i++;
-        }
-        else if(i<n && s.charAt(i)=='-'){
-            sign=-1;
-            i++;
+        if(i<n && (s.charAt(i)=='+' || s.charAt(i)=='-')){
+            sign=s.charAt(i)=='-'?-1:1;
+            i++; //read the sign
         }
       
         
         while(i<n && Character.isDigit(s.charAt(i))){
            sum=sum*10+s.charAt(i)-'0';
-           i++;
+           i++;  //read the digit
         }
 
         if(sign==-1){
-            sum=sum*-1;
+            sum=sum*-1; //check sign
         }
         sum= sum>=Integer.MAX_VALUE?Integer.MAX_VALUE:sum;
         sum= sum<=Integer.MIN_VALUE?Integer.MIN_VALUE:sum;
-        return (int)sum;
+        return (int)sum; //check sum
 
 
 
