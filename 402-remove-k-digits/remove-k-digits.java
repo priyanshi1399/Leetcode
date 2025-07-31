d801df0936a73eb0ab1965bed43e1b28dac26a74
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-07-31 22:45:04 +0530

    Time: 31 ms (18.64%) | Memory: 46.1 MB (18.58%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,29 +1,28 @@
 class Solution {
     public String removeKdigits(String num, int k) {
         int n=num.length();
-        Stack<Character> stck=new Stack<>();
-        StringBuilder result=new StringBuilder();
-        for(int i=0;i<n;i++){
-            while(!stck.isEmpty() && stck.peek()>num.charAt(i) && k>0){
-                stck.pop();
-                k--;
+        Stack<Integer> stck=new Stack<>();
+        int i=0;
+        while(i<n){
+            while(!stck.isEmpty() && stck.peek()>num.charAt(i)-'0' && k>0){
+                    stck.pop();
+                    k--;
             }
-
-            if(!stck.isEmpty() || num.charAt(i)!='0'){
-                stck.push(num.charAt(i));
+                if(!stck.isEmpty() || num.charAt(i)-'0'!=0){
+                  stck.push(num.charAt(i)-'0');
+                 }
+                 i++;
             }
-        }
+        
 
-        //if few elements left //123456 
-        while(!stck.isEmpty() && k>0){
-            stck.pop();
-            k--;
-        }
-
-        while(!stck.isEmpty()){
-            result.append(stck.pop());
-        }
-        return result.length()>0?result.reverse().toString():"0";
-    
+            StringBuilder str=new StringBuilder();
+            while(!stck.isEmpty() && k>0){ //special case where elements in stck and k is also left
+                    stck.pop();
+                    k--;
+            }
+            while(!stck.isEmpty()){
+                    str.append(stck.pop());
+            }
+            return str.length()==0?"0":str.reverse().toString();
     }
 }
