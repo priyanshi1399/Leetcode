03010b4cb7ec5aaba958780287b7f6c120641c36
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-01 02:21:33 +0530

    Time: 12 ms (88.70%) | Memory: 45.2 MB (71.83%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,28 +1,26 @@
 class Solution {
     public String removeKdigits(String num, int k) {
+        StringBuilder ans=new StringBuilder(); //take String for result
         int n=num.length();
-        Stack<Integer> stck=new Stack<>();
-        int i=0;
-        while(i<n){
-            while(!stck.isEmpty() && stck.peek()>num.charAt(i)-'0' && k>0){
-                    stck.pop();
-                    k--;
+        if(n==k) return "0"; //if length and k is same return "0"
+        for(int i=0;i<n;i++){
+            while(ans.length()>0 && ans.charAt(ans.length()-1)>num.charAt(i) && k>0){
+                ans.deleteCharAt(ans.length()-1); //remove number from last ->"14" and "3" is to come remove 4
+                k--; //decrease k value
             }
-                if(!stck.isEmpty() || num.charAt(i)-'0'!=0){
-                  stck.push(num.charAt(i)-'0');
-                 }
-                 i++;
-            }
-        
 
-            StringBuilder str=new StringBuilder();
-            while(!stck.isEmpty() && k>0){ //special case where elements in stck and k is also left
-                    stck.pop();
-                    k--;
+            if(ans.length()>0 || num.charAt(i)!='0'){
+                ans.append(num.charAt(i)); // number starts with 0 then ignore else put 
             }
-            while(!stck.isEmpty()){
-                    str.append(stck.pop());
-            }
-            return str.length()==0?"0":str.reverse().toString();
+        }
+
+        while(ans.length()>0 && k>0){
+            ans.deleteCharAt(ans.length()-1); // if numbers are in increasing order remove from last
+            k--;
+        }
+
+        return ans.length()>0 ?ans.toString() :"0"; //if empty return "0"
     }
+
+
 }
