b910faacef05ce0a43fb6313219da0cf51e8bbfe
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-01 13:25:31 +0530

    Time: 3 ms (19.56%) | Memory: 55.4 MB (74.71%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int [] prefixArray=new int[n];
-        int left=1; //take 1 in the start
-        prefixArray[0]=1;
-        for(int i=1;i<n;i++){ //populate the prefixProductArray
-            prefixArray[i]=nums[i-1]*left;
-            left=prefixArray[i];
-        }
-
-        int [] suffixArray=new int[n];
-        int right=1; //take 1 in the start
-        suffixArray[n-1]=1;
-        for(int i=n-2;i>=0;i--){
-            suffixArray[i]=right*nums[i+1];
-            right=suffixArray[i];
-        }
-
-        int [] result=new int[n];
+        int [] ans=new int[n];
+        Arrays.fill(ans,1);
+        int left=1;
         for(int i=0;i<n;i++){
-            result[i]=prefixArray[i]*suffixArray[i];
+            ans[i]=left*ans[i];
+            left=left*nums[i];
         }
-        return result;
+        int right=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i]=ans[i]*right;
+            right=right*nums[i];
+        }
+
+        return  ans;
     }
 }
