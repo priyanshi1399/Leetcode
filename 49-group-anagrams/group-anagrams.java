b37404cf4c35b3f07340e2b707785fcf1b577207
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-03 05:12:13 +0530

    Time: 9 ms (33.78%) | Memory: 48.6 MB (14.79%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,8 +1,19 @@
 class Solution {
     public String sorted(String str){
-        char [] charArray=str.toCharArray();
-        Arrays.sort(charArray);
-        return new String(charArray);
+       int [] indexArray=new int[26];
+       for(int i=0;i<str.length();i++){
+        indexArray[str.charAt(i)-'a']++;
+       }
+
+       //combining those character by index it will be in sorted Array
+       StringBuilder sortedString=new StringBuilder();
+       for(int i=0;i<26;i++){
+        if(indexArray[i]!=0){
+            int num=indexArray[i];
+            sortedString.append(String.valueOf((char)(i+'a')).repeat(indexArray[i]));
+       }
+    }
+    return sortedString.toString();
     }
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map=new HashMap<>();
