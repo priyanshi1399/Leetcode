89a166dbbb4f51cf7a31760aa1b5402c5657a49b
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-03 02:44:25 +0530

    Time: 3 ms (55.41%) | Memory: 53.1 MB (60.01%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,10 +2,10 @@ class Solution {
     public int majorityElement(int[] nums) {
         int n=nums.length;
         int count=0;
-        int majEle=0;
+        Integer majEle=null;
 
         for(int i=0;i<n;i++){
-            if(nums[i]==majEle){ //if it is the same element
+            if(majEle!=null && nums[i]==majEle){ //if it is the same element
                     count++;
             }
             else if(count==0){
