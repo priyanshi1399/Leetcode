349a8079e5c98456120153faae8951779f9e5bdb
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-04 16:37:29 +0530

    Time: 3 ms (91.92%) | Memory: 45.3 MB (6.64%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,28 +3,17 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character,Integer> sMap=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            sMap.put(ch,sMap.getOrDefault(ch,0)+1);
-        }
-        HashMap<Character,Integer> tMap=new HashMap<>();
-         for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            tMap.put(ch,tMap.getOrDefault(ch,0)+1);
-        }
+        char [] sArray=s.toCharArray();
+        char  []tArray=t.toCharArray();
 
-       for(Map.Entry<Character,Integer> entry:sMap.entrySet()){
-        char key=entry.getKey();
-        int val=entry.getValue();
-        if(!tMap.containsKey(key)){
-            return false;
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+
+        for(int i=0;i<sArray.length;i++){
+            if(sArray[i]!=tArray[i]){
+                return false;
+            }
         }
-        else if(tMap.get(key)!=val){
-            return false;
-        }
-       }
         return true;
-       
     }
 }
