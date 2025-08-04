3f08ce9ac25c98b7da604ba409375ce37a99257b
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-05 03:59:09 +0530

    Time: 11 ms (2.24%) | Memory: 57.2 MB (42.60%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,7 +5,7 @@ class Solution {
         int sum=0;
         int startIndex=0;
         int ansStart=0;
-        int endIndex=0;
+        int ansEnd=0;
         for(int i=0;i<n;i++){
     
             if(sum<0){
@@ -13,13 +13,11 @@ class Solution {
                 startIndex=i+1<n?i+1:i; //additional if asked to print the range
             }
             sum+=nums[i];
-            if(sum>maxSum){
-                maxSum=sum;
-                ansStart=startIndex;
-                endIndex=i;
-            }
+            maxSum=Math.max(maxSum,sum);
+            ansStart=startIndex;
+            ansEnd=i;
         }
-        System.out.println("Starting array Index is"+ ansStart +"ending array Index"+endIndex);
+        System.out.println("Starting array Index is"+ ansStart +"ending array Index"+ansEnd);
         return maxSum;
     }
 }
