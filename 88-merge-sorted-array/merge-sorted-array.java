4495eb93d4373ccc13d873f9a22d17c7ca8d6997
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-04 13:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (60.66%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,21 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i=m-1;
-        int j=n-1;
-        int k=m+n-1;
-    while(i>=0 && j>=0){
-        if(nums1[i]<=nums2[j]){
-            nums1[k]=nums2[j];
-            k--;
-            j--;
-        }
-        else{
-            nums1[k]=nums1[i];
-            k--;
-            i--;
-        }
-    }
+        int i=m-1,j=n-1,k=m+n-1;
 
+        while(i>=0 && j>=0){
+            if(nums1[i]<=nums2[j]){
+                nums1[k]=nums2[j];
+                k--;
+                j--;
+            }
+            else{
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }
+        }
         while(i>=0){
             nums1[k]=nums1[i];
             k--;
@@ -26,39 +24,7 @@ class Solution {
             k--;
             j--;
         }
-    
-    
     }
 }
 
 
-/*nums1 = [1,2,2,3,5,6]
-           ik
-int i=m-1
-int j=n-1
-int k=m+n-1
-
- nums2 = [2,5,6]
-          
-
-while(i>=0 && j>=0){
-if(nums[i]<=nums[j]){
-    nums[k]=nums[j];
-    k--;
-    j--;
-}
-else{
-    nums[k]=nums[i];
-    k--;
-    i--;
-}
-
-
-while(i>=0){
-    nums[k]=nums[i];
-    k--;
-    i--;
-}
-while(j>=0){
-
-}*/
