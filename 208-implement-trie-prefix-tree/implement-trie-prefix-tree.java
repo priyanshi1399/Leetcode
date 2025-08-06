c0ba12d4485a8707d17c1167253be4dd2b86da1e
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-06 07:30:57 +0530

    Time: 36 ms (54.45%) | Memory: 55.4 MB (56.39%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,53 +1,41 @@
-class Trie {
-    static class TrieNode{
-        boolean endOfWord;
-        TrieNode [] children;
-
-        public TrieNode(){
-            endOfWord=false; //this will tell whether any word have ended on it
-            children=new TrieNode[26]; //it will be all lowercase letter
-        }
+class TrieNode{
+    boolean endOfWord;
+    TrieNode [] children;
+    public TrieNode() {
+        endOfWord=false; //check at the end
+        children=new TrieNode[26]; //taking 26 letters of array
     }
 
-    private TrieNode root;
-
-    public Trie() {
-        root=new TrieNode();
+}
+class Trie {
+    TrieNode root;
+    public Trie(){
+            root=new TrieNode();
+    }
+    public TrieNode getTrieNode(){
+        return new TrieNode();
     }
     
-    private TrieNode getTrieNode(){ //making anew TrieNode
-            return new TrieNode();
-    }
+    
     public void insert(String word) {
         TrieNode crawl=root;
-
         for(int i=0;i<word.length();i++){
-            char ch=word.charAt(i);
-
-            int idx=ch-'a';
-
-            if(crawl.children[idx]==null){ //if node not exist for that charcater
-                crawl.children[idx]=getTrieNode();          //create a new Node
+            int idx=word.charAt(i)-'a';
+            if(crawl.children[idx]==null){
+                crawl.children[idx]=getTrieNode();
             }
-
             crawl=crawl.children[idx];
         }
-        //at teh end mark endOfWord=true;
         crawl.endOfWord=true;
-        }
-
+    }
     
     public boolean search(String word) {
-        TrieNode crawl=root;
-
+         TrieNode crawl=root;
         for(int i=0;i<word.length();i++){
-            char ch=word.charAt(i);
-            int idx=ch-'a';
-
+            int idx=word.charAt(i)-'a';
             if(crawl.children[idx]==null){
-                return false; //that node index also dosent exist
+                return false;
             }
-
             crawl=crawl.children[idx];
         }
         if(crawl.endOfWord==true){
@@ -57,23 +45,15 @@ class Trie {
     }
     
     public boolean startsWith(String prefix) {
-        TrieNode crawl=root;
-
-        int i=0;
-        for(;i<prefix.length();i++){
-            char ch=prefix.charAt(i);
-            int idx=ch-'a';
-
+          TrieNode crawl=root;
+        for(int i=0;i<prefix.length();i++){
+            int idx=prefix.charAt(i)-'a';
             if(crawl.children[idx]==null){
                 return false;
             }
-
             crawl=crawl.children[idx];
         }
-        if(i==prefix.length()){
-            return true;
-        }
-        return false;
+        return true;
     }
 }
 
