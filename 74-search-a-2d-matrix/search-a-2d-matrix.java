521b3d0b6f95b91a30c67778439a451dcbf9a120
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-08 04:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (65.61%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -22,10 +22,10 @@ class Solution {
         int n=matrix[0].length;  //size of column
 
         for(int i=0;i<m;i++){
-            if(target>=matrix[i][0] && target<=matrix[i][n-1]){
-                return binarySearch(matrix[i],target);
+                if(binarySearch(matrix[i],target)){
+                    return true;
+                }
             }
-        }
 
         return false;
     }
