2d84e4896c7c94390f7ad2da7c0c59dafbf019ee
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-07 18:50:37 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (72.99%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,54 +1,66 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        //first choose the lesser size array 
-        //find by recusrion by swapping
-
-        //l=0,r=m ; taken all element (m)
-        //mid(px)  and take Py for nums2->(m+n+1)/2-px
-
-        //calculate x1,x2,x3,x4 calculate median by checking size
-        //if x>x4 h=mid-1 else l=mid+1
-
-        int m=nums1.length;
-        int n=nums2.length;
-        if(m+n==1){
-            return (double)nums1.length==1?nums1[0]:nums2[0];
-        }
-        if(nums1.length>nums2.length){
-            return findMedianSortedArrays(nums2,nums1);
-        }
-
-        int l=0;
-        int r=m;
-        while(l<=r){
-            int Px=l+(r-l)/2; //for nums1
-            int Py=(m+n+1)/2-Px; //for nums2
-            //left half
-            int x1= (Px==0)?Integer.MIN_VALUE:nums1[Px-1]; //in this part its last element
-            int x3= (Px==m)?Integer.MAX_VALUE:nums1[Px];
-            
-
-            //right half
-            int x2= (Py==0)?Integer.MIN_VALUE:nums2[Py-1];
-            int x4= (Py==n)?Integer.MAX_VALUE:nums2[Py];
-
-            if(x1<=x4 && x2<=x3){
-                if((m+n)%2==1){ //if its odd 
-                    return (double)Math.max(x1,x2);
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int mergedLen=n1+n2;
+        int i=0;
+        int j=0;
+        int k=0;
+        int idx1=(mergedLen/2)-1;
+        int idx2=mergedLen/2;
+        int element1=0,element2=0;
+        while(i<n1 && j<n2){
+            if(nums1[i]<=nums2[j]){
+                if(k==idx1){
+                    element1=nums1[i];
                 }
-                else{
-                    return (Math.max(x1,x2)+Math.min(x3,x4))/2.0;
+                if(k==idx2){
+                    element2=nums1[i];
                 }
-            }
-            else if(x1>x4){ //taken more elments
-                r=Px-1;
+                k++;
+                i++;
             }
             else{
-                l=Px+1;
+                
+                if(k==idx1){
+                    element1=nums2[j];
+                }
+                if(k==idx2){
+                    element2=nums2[j];
+                }
+                k++;
+                j++;
             }
-
         }
-        return -1;
+        while(i<n1){
+            if(k==idx1){
+                    element1=nums1[i];
+                }
+                if(k==idx2){
+                    element2=nums1[i];
+                }
+                k++;
+                i++;
+        }
+
+        while(j<n2){
+                if(k==idx1){
+                        element1=nums2[j];
+                }
+                if(k==idx2){
+                    element2=nums2[j];
+                }
+                k++;
+                j++;
+        }
+        double median=0.0;
+        if(mergedLen%2==0){
+            median=(element1+element2)/2.0;
+        }
+        else{
+            median=element2;
+        }
+        return (double)median;
 
     }
 }
