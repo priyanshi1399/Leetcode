dddc610e312a64946ece3413699b1f58d98fb3e8
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-07 08:20:12 +0530

    Time: 2 ms (55.81%) | Memory: 46.2 MB (60.14%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -6,30 +6,62 @@ class Solution {
         int i=0;
         int j=0;
         int k=0;
+        int idx1=(mergedArray.length/2-1);
+        int idx2=mergedArray.length/2;
+        int element1=0,element2=0;
         while(i<n1 && j<n2){
             if(nums1[i]<=nums2[j]){
-                mergedArray[k++]=nums1[i++];
+                if(k==idx1){
+                    element1=nums1[i];
+                }
+                if(k==idx2){
+                    element2=nums1[i];
+                }
+                k++;
+                i++;
             }
             else{
-                mergedArray[k++]=nums2[j++];
+                
+                if(k==idx1){
+                    element1=nums2[j];
+                }
+                if(k==idx2){
+                    element2=nums2[j];
+                }
+                k++;
+                j++;
             }
         }
         while(i<n1){
-            mergedArray[k++]=nums1[i++];
+            if(k==idx1){
+                    element1=nums1[i];
+                }
+                if(k==idx2){
+                    element2=nums1[i];
+                }
+                k++;
+                i++;
         }
 
         while(j<n2){
-            mergedArray[k++]=nums2[j++];
+                if(k==idx1){
+                        element1=nums2[j];
+                }
+                if(k==idx2){
+                    element2=nums2[j];
+                }
+                k++;
+                j++;
         }
         double median=0.0;
         int len=mergedArray.length;
         if(len%2==0){
-            median=((double)mergedArray[(len/2)-1]+mergedArray[len/2])/2;
+            median=(element1+element2)/2.0;
         }
         else{
-            median=(double)mergedArray[len/2];
+            median=element2;
         }
-        return median;
+        return (double)median;
 
     }
 }
