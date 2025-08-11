2e3d5ff4cc4c85c9acfaac13a5345de8df6cb882
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-11 12:50:50 +0530

    Time: 1 ms (72.39%) | Memory: 45.5 MB (93.94%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,23 +2,46 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int [] row=new int[m];
-        int [] col=new int[n];
+        int col0=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1; //mark the row as 1 which is 0 in matrix
-                    col[j]=1; //mark the col as 1 which is 1 in matrix
+                    //mark the row as 0
+                    matrix[i][0]=0;
+                    //mark the column as 0
+                    if(j!=0){  
+                    matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
                 }
             }
         }
 
-         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-               if(row[i]==1 || col[j]==1){
-                matrix[i][j]=0;
-               }
+        //traverse without first row and first column
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
             }
         }
+
+        //now come to first row and first column first change the column then row
+        if(matrix[0][0]==0){ //makrk whole first column 0
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        } //now come to first row
+        if(col0==0){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+
+
     }
 }
