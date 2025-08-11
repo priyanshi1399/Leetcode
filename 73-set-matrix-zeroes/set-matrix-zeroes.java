b88bb587d7ccc2b95ae4d4343477c2831a128467
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-11 17:10:50 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (66.89%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,41 +2,52 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int col0=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    //mark the row as 0
-                    matrix[i][0]=0;
-                    //mark the column as 0
-                    if(j!=0){  
-                    matrix[0][j]=0;
-                    }
-                    else{
-                        col0=0;
-                    }
+        boolean isfirstRowImpacted =false;
+        boolean isfirstColImpacted=false;
+
+            //check if row is Impacted
+            for(int col=0;col<n;col++){
+                if(matrix[0][col]==0){
+                    isfirstRowImpacted=true;
+                    break;
                 }
             }
+            //check if column is impacted
+
+        for(int row=0;row<m;row++){
+                if(matrix[row][0]==0){
+                    isfirstColImpacted=true;
+                    break;
+                }
         }
 
-        //traverse without first row and first column
+
+        //set markers in first row and first column
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(matrix[i][j]!=0){
-                    if(matrix[i][0]==0 || matrix[0][j]==0){
-                        matrix[i][j]=0;
-                    }
+                  if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                  }
+                }
+            }
+
+        //set other row col 0 if first row or first col=0
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
+                  matrix[i][j]=0;
                 }
             }
         }
 
-        //now come to first row and first column first change the column then row
-        if(matrix[0][0]==0){ //makrk whole first column 0
+        if(isfirstRowImpacted){
             for(int j=0;j<n;j++){
                 matrix[0][j]=0;
             }
-        } //now come to first row
-        if(col0==0){
+        }
+
+        if(isfirstColImpacted){
             for(int i=0;i<m;i++){
                 matrix[i][0]=0;
             }
