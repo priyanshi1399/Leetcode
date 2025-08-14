072bdbf1a8bc2270e847054899f422a8f43d1a34
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-14 19:55:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (46.24%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,14 +1,14 @@
 class Solution {
     public void solve(int idx,int [] nums, int n, List<Integer> temp,List<List<Integer>> res){
-        if(idx>=n){
-            res.add(new ArrayList<>(temp));
-            return;
+        if(idx>=n){ //index reaches till length
+            res.add(new ArrayList<>(temp)); //add into final list
+            return; 
         }
 
-        temp.add(nums[idx]);
-        solve(idx+1,nums,n,temp,res);
-        temp.remove(temp.size()-1);
-        solve(idx+1,nums,n,temp,res);
+        temp.add(nums[idx]); //do add
+        solve(idx+1,nums,n,temp,res); //explore to next index
+        temp.remove(temp.size()-1); //remove other element
+        solve(idx+1,nums,n,temp,res); //explore after removing the element
     }
     public List<List<Integer>> subsets(int[] nums) {
         int n=nums.length;
