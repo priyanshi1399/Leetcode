0a67b4826f21e6e83781175a7c47cb07f8905353
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-14 20:40:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (65.56%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,17 @@
 class Solution {
-    public void solve(int idx,int [] nums, int n, List<Integer> temp,List<List<Integer>> res){
-        if(idx>=n){ //index reaches till length
-            res.add(new ArrayList<>(temp)); //add into final list
-            return; 
-        }
-
-        temp.add(nums[idx]); //do add
-        solve(idx+1,nums,n,temp,res); //explore to next index
-        temp.remove(temp.size()-1); //remove other element
-        solve(idx+1,nums,n,temp,res); //explore after removing the element
-    }
     public List<List<Integer>> subsets(int[] nums) {
         int n=nums.length;
-        List<List<Integer>> res=new ArrayList<>();
-        solve(0,nums,n,new ArrayList<>(),res);
+        List<List<Integer>> res=new ArrayList<>(); // result list
+        int subLimit=(int)Math.pow(2,n); // limit till here subset will be formed
+        for(int num=0;num<subLimit;num++){
+            List<Integer> temp=new ArrayList<>(); //taking temporary list
+            for(int bitIndex=0;bitIndex<n;bitIndex++){
+                if((num & (1<<bitIndex))!=0){ //checking bit is set 
+                    temp.add(nums[bitIndex]); //include that index element
+                }
+            }
+            res.add(temp);
+        }
         return res;
     }
 }
