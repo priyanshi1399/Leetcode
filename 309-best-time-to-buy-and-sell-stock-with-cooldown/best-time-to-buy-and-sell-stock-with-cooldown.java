0750627f2fc67eaae9cd2106bef244b7ca688ffe
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-15 16:54:06 +0530

    Time: 13 ms (5.38%) | Memory: 104.9 MB (5.05%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -5,15 +5,15 @@ class Solution {
             return 0;
         }
         if(dp[idx][buy]!=-1){
-            return dp[idx][buy];
+            return dp[idx][buy]; 
         }
 
         if(buy==1){
-            maxProfit=Math.max(-prices[idx]+func(idx+1,0,maxProfit,prices,n,dp),func(idx+1,1,maxProfit,prices,n,dp));
+            maxProfit=Math.max(-prices[idx]+func(idx+1,0,maxProfit,prices,n,dp),func(idx+1,1,maxProfit,prices,n,dp)); //here if we are buying then 0 means we can not buy again and if we are  not buying then 1 means we can buy again
 
         }
         else{
-            maxProfit=Math.max(+prices[idx]+func(idx+2,1,maxProfit,prices,n,dp),func(idx+1,0,maxProfit,prices,n,dp));
+            maxProfit=Math.max(+prices[idx]+func(idx+2,1,maxProfit,prices,n,dp),func(idx+1,0,maxProfit,prices,n,dp)); //here we are seeling the stocks and moving to idx+2 due to cooldown and when moving to idx+1 still not selling and can not buy it due to cooldown 
 
         }
         return dp[idx][buy]=maxProfit;
