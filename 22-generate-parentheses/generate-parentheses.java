e6e8bb142daf8765c915832763d84566c578903d
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-16 14:00:29 +0530

    Time: 6 ms (7.00%) | Memory: 43.3 MB (46.91%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -2,43 +2,37 @@ class Solution {
     public boolean isValid(StringBuilder curr){
         int sum=0;
         for(int i=0;i<curr.length();i++){
+            if(sum<0){
+                return false;
+            }
             if(curr.charAt(i)=='('){
                 sum++;
             }
             else{
                 sum--;
             }
-        if(sum<0){
-            return false;
-        }
         }
         return sum==0;
     }
-    public void solve(StringBuilder curr, List<String> res, int n){
-
+    public void solve(List<String> res, StringBuilder curr, int n){
         if(curr.length()==2*n){
-            System.out.println(curr.toString());
             if(isValid(curr)){
-            res.add(curr.toString());
-            
+                res.add(curr.toString());
             }
             return;
-            
         }
 
-        curr=curr.append('(');
-        solve(curr, res,n);
+        curr.append('(');
+        solve(res,curr,n);
         curr.deleteCharAt(curr.length()-1);
-
-        curr=curr.append(')');
-        solve(curr, res,n);
+        curr.append(')');
+        solve(res,curr,n);
         curr.deleteCharAt(curr.length()-1);
     }
     public List<String> generateParenthesis(int n) {
         StringBuilder curr=new StringBuilder();
-
         List<String> res=new ArrayList<>();
-        solve(curr,res,n);
+        solve(res,curr,n);
         return res;
     }
 }
