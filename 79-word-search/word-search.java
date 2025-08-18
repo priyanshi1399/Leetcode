a9d803c17ee25cfc43d04b3fec6b1648c0d665b7
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-18 23:09:43 +0530

    Time: 182 ms (28.34%) | Memory: 41.4 MB (95.81%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,45 +1,51 @@
 class Solution {
     int m;
     int n;
-    private int [][] directions={{0,1},{0,-1},{1,0},{-1,0}};
-    public boolean isSafe(int x,int y, char [][] board){
+    private static final int  [][] direction={{-1,0},{1,0},{0,1},{0,-1}};
+    public boolean isSafe(int x,int y,int m,int n,char [][] board){
         if(x<0 || y<0 || x>=m || y>=n){
-            return false;
+                return false;
         }
         return true;
     }
-    public boolean solve(int x,int y,int index,char [][] board,String word,boolean [][] visited){
-        if(index==word.length()-1){
+    public boolean solve(int x,int y,int index,boolean [][] visited,char [][] board,int len,String word){
+        if(board[x][y]==word.charAt(word.length()-1) && index==word.length()-1){
             return true;
         }
         visited[x][y]=true;
-        for(int [] dir: directions){
+        for(int [] dir:direction){
             int x_=x+dir[0];
             int y_=y+dir[1];
 
-            if(isSafe(x_,y_,board) && !visited[x_][y_] &&  board[x_][y_]==word.charAt(index+1)){
-                    if(!visited[x_][y_] && solve(x_,y_,index+1,board,word,visited)){
-                        return true;
-                    }
+            if((isSafe(x_,y_,m,n,board)) && (!visited[x_][y_]) && (board[x_][y_]==word.charAt(index+1))){
+                if(!visited[x_][y_] && solve(x_,y_,index+1,visited,board,len, word)){
+                    return true;
                 }
+            }
+
+
         }
         visited[x][y]=false;
         return false;
+
+
     }
+
     public boolean exist(char[][] board, String word) {
         m=board.length;
         n=board[0].length;
-        boolean [][] visited=new boolean [m][n];
+        int len=word.length();
+        boolean[][] visited=new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!visited[i][j] && board[i][j]==word.charAt(0)){
-                    if(solve(i,j,0,board,word,visited)){
+                if((!visited[i][j]) && (board[i][j]==word.charAt(0))){
+                    if(solve(i,j,0,visited,board,len,word)){
                         return true;
                     }
+                        
                 }
             }
         }
         return false;
-        
     }
 }
