e1f5e874211d8a57d105d55060f844f63ba9dacb
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-19 04:47:38 +0530

    Time: 2 ms (86.26%) | Memory: 45.1 MB (38.07%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,49 +1,62 @@
 class Solution {
-     Set<Integer> cols=new HashSet<>();//J
-        Set<Integer> diag=new HashSet<>(); //row+j
-        Set<Integer> antidiag=new HashSet<>(); //row-j
-        List<List<String>> result=new ArrayList<>();
-    public void solve(int row,List<String> board,int n){
-        if(row>=n){
-            result.add(new ArrayList<>(board));
+    public boolean isSafe(int row,int col,char[][] board,int n){
+
+        //check for column
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false; // we can't place the queen
+            }
+        }
+
+
+        //check for upper right diagonal (row-- col++)
+        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+
+        //check for left diagonal (row--,col--)
+        for(int i=row-1,j=col-1;i>=0 && j>=0; i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+    public List<String> addPath(char [][] board){
+        List<String> path=new ArrayList<>();
+        for(int i=0;i<board.length;i++){
+            path.add(new String(board[i]));
+        }
+        return path;
+    }
+    public void backtrack(int row,char [][] board, List<List<String>> res,int n){
+        if(row==n){
+            res.add(addPath(board));
             return;
         }
 
-        for(int j=0;j<n;j++){
-            
-            if(cols.contains(j) || diag.contains(row+j) || antidiag.contains(row-j)){
-                continue;
-            }
-            cols.add(j);
-            diag.add(row+j);
-            antidiag.add(row-j);
 
-            char [] charArray=board.get(row).toCharArray();
-            charArray[j]='Q';
-            String replace=new String(charArray);
-            board.set(row,replace);
-            solve(row+1,board,n);
-            cols.remove(j);
-            diag.remove(row+j);
-            antidiag.remove(row-j);
-            charArray[j]='.';
-            replace=new String(charArray);
-            board.set(row,replace);
-           
+        for(int col=0;col<n;col++){
+            if(isSafe(row,col,board,n)){
+                board[row][col]='Q';
+                backtrack(row+1,board,res,n);
+                board[row][col]='.';
+            }
         }
     }
     public List<List<String>> solveNQueens(int n) {
-
-        List<String> board=new ArrayList<>();
+        List<List<String>> res=new ArrayList<>();
+        char [][] board=new char[n][n];
         for(int i=0;i<n;i++){
-            StringBuilder curr=new StringBuilder();
-            for(int j=0;j<n;j++){
-                curr.append('.');
-            }
-            board.add(curr.toString());
+            Arrays.fill(board[i],'.');
         }
 
-        solve(0,board,n);
-        return result;
+        backtrack(0,board,res,n);
+        return res;
+
     }
 }
