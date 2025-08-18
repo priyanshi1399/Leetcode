14f461e0bec2d03aa349daf606cd3332b38db945
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-19 02:17:42 +0530

    Time: 149 ms (43.58%) | Memory: 42.4 MB (21.71%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,37 +1,49 @@
 class Solution {
-    public boolean solve(int x,int y, int index, char [][] board, String word){
-        
-        if(index==word.length()){
+    int m;
+    int n;
+    private static final int  [][] direction={{-1,0},{1,0},{0,1},{0,-1}};
+    public boolean isSafe(int x,int y,int m,int n,char [][] board){
+        if(x<0 || y<0 || x>=m || y>=n){
+                return false;
+        }
+        return true;
+    }
+    public boolean solve(int x,int y,int index,char [][] board,int len,String word){
+        if(index == word.length() - 1) {
             return true;
         }
-        if(x>=board.length || y>=board[0].length || x<0 || y<0 || board[x][y]!=word.charAt(index) || board[x][y]=='$'){
-            return false;
-        }
-
-        
-
         char temp=board[x][y];
-        board[x][y]='$'; //replace with any character
+        board[x][y]='$';
+        for(int [] dir:direction){
+            int x_=x+dir[0];
+            int y_=y+dir[1];
 
-        if(solve(x+1,y,index+1,board,word) || 
-        solve(x-1,y,index+1,board,word) || 
-        solve(x,y+1,index+1,board,word) || 
-        solve(x,y-1,index+1,board,word) ){
-            return true;
+            if((isSafe(x_,y_,m,n,board)) && (board[x_][y_]!='$') && (board[x_][y_]==word.charAt(index+1))){
+                if(board[x_][y_]!='$' && solve(x_,y_,index+1,board,len, word)){
+                    board[x][y]='$';
+                    return true;
+                }
+            }
+
+
         }
-
         board[x][y]=temp;
         return false;
+
+
     }
+
     public boolean exist(char[][] board, String word) {
-        int m=board.length;
-        int n=board[0].length;
+        m=board.length;
+        n=board[0].length;
+        int len=word.length();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(solve(i,j,0,board,word)){
+                    if(solve(i,j,0,board,len,word)){
                         return true;
                     }
+                        
                 }
             }
         }
