20384af53b8e9be878bfd4a4a21a2f229087ba45
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-19 04:55:56 +0530

    Time: 4 ms (34.85%) | Memory: 44.9 MB (75.26%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,31 +1,7 @@
 class Solution {
-    public boolean isSafe(int row,int col,char[][] board,int n){
-
-        //check for column
-        for(int i=row-1;i>=0;i--){
-            if(board[i][col]=='Q'){
-                return false; // we can't place the queen
-            }
-        }
-
-
-        //check for upper right diagonal (row-- col++)
-        for(int i=row-1,j=col+1;i>=0 && j<n;i--,j++){
-            if(board[i][j]=='Q'){
-                return false;
-            }
-        }
-
-        //check for left diagonal (row--,col--)
-        for(int i=row-1,j=col-1;i>=0 && j>=0; i--,j--){
-            if(board[i][j]=='Q'){
-                return false;
-            }
-        }
-        return true;
-
-
-    }
+    Set<Integer> col=new HashSet<>();
+    Set<Integer> diagonal=new HashSet<>();
+    Set<Integer> antiDiagonal=new HashSet<>();
     public List<String> addPath(char [][] board){
         List<String> path=new ArrayList<>();
         for(int i=0;i<board.length;i++){
@@ -39,14 +15,22 @@ class Solution {
             return;
         }
 
-
-        for(int col=0;col<n;col++){
-            if(isSafe(row,col,board,n)){
-                board[row][col]='Q';
-                backtrack(row+1,board,res,n);
-                board[row][col]='.';
-            }
+        for(int j=0;j<n;j++){
+        if(col.contains(j) || diagonal.contains(row+j) || antiDiagonal.contains(row-j)){
+            continue; //if those values already exists in set
         }
+        board[row][j]='Q';
+        col.add(j);
+        diagonal.add(row+j);
+        antiDiagonal.add(row-j);
+        backtrack(row+1,board,res,n);
+        board[row][j]='.';
+        col.remove(j);
+        diagonal.remove(row+j);
+        antiDiagonal.remove(row-j);
+
+        }
+       
     }
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> res=new ArrayList<>();
