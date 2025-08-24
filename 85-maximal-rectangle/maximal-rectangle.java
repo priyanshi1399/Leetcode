b9c59d1398983b53c50ce4e0a1de0caf26a41ddc
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-24 22:00:16 +0530

    Time: 16 ms (57.51%) | Memory: 46.6 MB (41.31%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -1,58 +1,57 @@
 class Solution {
+    public int findMaximalRectangle(int [] area){
+        int n=area.length;
+        int maxArea=0;
+        Stack<Integer> stck=new Stack<>();
+        for(int i=0;i<n;i++){
+
+            while(!stck.isEmpty() && area[stck.peek()]>=area[i]){
+
+                int index=stck.pop();
+
+                int nse=i;
+                int pse=stck.isEmpty()?-1:stck.peek();
+                maxArea=Math.max(maxArea,area[index]*(nse-pse-1));
+            }
+            stck.push(i);
+        }
+
+             while(!stck.isEmpty()){
+
+                int index=stck.pop();
+
+                int nse=n;
+                int pse=stck.isEmpty()?-1:stck.peek();
+                maxArea=Math.max(maxArea,area[index]*(nse-pse-1));
+            }
+            return maxArea;
+        }
+    
     public int maximalRectangle(char[][] matrix) {
-        int m=matrix.length;
+          int m=matrix.length;
         int n=matrix[0].length;
         int maxArea=0;
         int [][] prefixSum=new int[m][n];
         
         for(int j=0;j<n;j++){
             int sum=0;
-            for(int i=0;i<m;i++){  
-                sum+=matrix[i][j]-'0';              
-               if(matrix[i][j]=='0'){
-                prefixSum[i][j]=0;
-                sum=0;
-               }
-               else{
-                prefixSum[i][j]=sum;
-               }
-            }          
+            for(int i=0;i<m;i++){
+                sum+=matrix[i][j]-'0';
+                if(matrix[i][j]=='0'){
+                    sum=0;
+                    prefixSum[i][j]=sum;
+                }
+                else{
+                    prefixSum[i][j]=sum;
+                }
+            }
+        
         }
 
         for(int i=0;i<m;i++){
-            
-            maxArea=Math.max(maxArea,findMaxRectangle(prefixSum[i]));
+            maxArea=Math.max(maxArea,findMaximalRectangle(prefixSum[i]));
         }
-        return maxArea;
-    }
-
-        public int findMaxRectangle(int [] area){
-            int n=area.length;
-        Stack<Integer> stck=new Stack<>();
-        int nse,pse;
-        int maxArea=0;
-        for(int i=0;i<n;i++){
-            while(!stck.isEmpty() && area[stck.peek()]>=area[i]){
-
-               int  index=stck.pop();
-
-                nse=i;
-                pse=stck.isEmpty()?-1:stck.peek();
-                maxArea=Math.max(maxArea,(area[index]*(nse-pse-1)));
-                
-            }
-            stck.push(i);
-        }
-        
-        while(!stck.isEmpty()){
-            int index=stck.pop();
-            nse=n;
-            pse=stck.isEmpty()?-1:stck.peek();
-            maxArea=Math.max(maxArea,(area[index]*(nse-pse-1)));
-        }
-
-        return maxArea;
-
+        return maxArea;  
 
     }
 }
