7906beda299c812a9b79e0d3bae007d42c3ff473
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-31 03:02:02 +0530

    Time: 1 ms (65.01%) | Memory: 42.4 MB (23.95%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -21,34 +21,25 @@ class Solution {
         }
         Queue<TreeNode> q=new LinkedList<>();
         q.add(root);
-        boolean flag=false;
+        boolean leftToRight=false;
         while(!q.isEmpty()){
             int n=q.size(); //n=1 ,n=2
-            List<Integer> temp=new ArrayList<>(); //[]
-            while(n>0){ //1>0 ,2>0 ,2>0
+            List<Integer> temp=new ArrayList<>(Collections.nCopies(n,0)); //[]
+            for(int i=0;i<n;i++){ //1>0 ,2>0 ,2>0
                 TreeNode curr=q.poll(); //curr=3  curr=[9] curr=20 ,curr=15 ,curr=7
-                temp.add(curr.val); //temp-[3] ,temp-[9,20], temp-[15,7]
+                int index=leftToRight==false?i:n-1-i;
+                temp.set(index,curr.val); //temp-[3] ,temp-[9,20], temp-[15,7]
+
                 if(curr.left!=null){
                     q.add(curr.left); //1=[9]  q=[15]
                 }
                 if(curr.right!=null){
                     q.add(curr.right); //q=[9,20] q.size()=2; ,q=[15,7]
                 }
-                n--; //n=0 , n=1 n=0 ,n=1 n=0
+                
             }
-            if(flag){
-                int len=temp.size();
-                Integer [] arr=new Integer[len];
-                int index=0;
-                for(int i=len-1;i>=0;i--) {//[20,9]
-                    arr[index++]=temp.get(i);
-                }
-                res.add(Arrays.asList(arr)); // will be added in reverse [20,9]
-            }
-            else{
-                res.add(temp);// [[3], [15,7]]
-            }
-            flag=!flag; //false //true
+            res.add(temp);
+            leftToRight=!leftToRight; //false //true
             
             
         }
