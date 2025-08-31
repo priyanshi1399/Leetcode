f3cdb6d2cb1a364432b3803a9e1748c1ae3d7ed0
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-31 15:46:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -1,16 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for JAVA
-
-import java.io.*;
-import java.util.*;
-import java.util.LinkedList;
-import java.util.Queue;
-
-
-// } Driver Code Ends
-
-// User function Template for Java
-
 /*
 class Node{
     int data;
@@ -23,160 +10,53 @@ class Node{
     }
 }
 */
-
+class Pair<K,V>{
+    private final K key;
+    private final V value;
+    
+    public Pair(K key,V value){
+        this.key=key;
+        this.value=value;
+        
+    }
+    public K getKey(){
+        return key;
+    }
+    public V getValue(){
+        return value;
+    }
+    
+}
 class Solution {
     // Function to return a list of nodes visible from the top view
     // from left to right in Binary Tree.
     static ArrayList<Integer> topView(Node root) {
-        ArrayList<Integer> result=new ArrayList<>();
+        TreeMap<Integer,Integer> map=new TreeMap<>();
         Queue<Pair<Node,Integer>> q=new LinkedList<>();
         if(root==null){
-            return result;
+            return new ArrayList<>();
         }
-        TreeMap<Integer,Integer> map=new TreeMap<>();
-        q.add(new Pair<>(root,0)); //pushing the root
+        q.add(new Pair<>(root,0)); //insert root and level 0
         while(!q.isEmpty()){
-            Pair<Node,Integer> pair=q.poll();
-            Node node=pair.getKey();
-            int line=pair.getValue();
-            
-            if(!map.containsKey(line)){
-                map.put(line,node.data);
+            int n=q.size();
+            while(n-->0){
+                Pair<Node,Integer> pair=q.poll();
+                int level=pair.getValue();
+                Node currNode=pair.getKey();
+                if(!map.containsKey(level)){
+                    map.put(level,currNode.data);
+                }
+                if(currNode.left!=null){
+                    q.add(new Pair<>(currNode.left,level-1));
+                }
+                if(currNode.right!=null){
+                    q.add(new Pair<>(currNode.right,level+1));
+                }
             }
-            
-            if(node.left!=null){
-                q.add(new Pair<>(node.left,line-1));
-            }
-            if(node.right!=null){
-                q.add(new Pair<>(node.right,line+1));
-            }
         }
-        for(int value:map.values()){
-            result.add(value);
-        }
-        return result;
+        return new ArrayList<>(map.values());
         
-    }
-    public  static class Pair<F,S>{
-        private final F First;
-        private final S Second;
-        
-        Pair(F first, S second){
-            this.First=first;
-            this.Second=second;
-        }
-        
-        public F getKey(){
-            return First;
-        }
-        public S getValue(){
-            return Second;
-        }
+       
         
     }
 }
-
-
-//{ Driver Code Starts.
-
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-
-public class Tree {
-
-    static Node buildTree(String str) {
-
-        if (str.length() == 0 || str.charAt(0) == 'N') {
-            return null;
-        }
-
-        String ip[] = str.split(" ");
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-
-        Queue<Node> queue = new LinkedList<>();
-
-        queue.add(root);
-        // Starting from the second element
-
-        int i = 1;
-        while (queue.size() > 0 && i < ip.length) {
-
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-
-            // Get the current node's value from the string
-            String currVal = ip[i];
-
-            // If the left child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-
-            // For the right child
-            i++;
-            if (i >= ip.length) break;
-
-            currVal = ip[i];
-
-            // If the right child is not null
-            if (!currVal.equals("N")) {
-
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-
-        return root;
-    }
-
-    static void printInorder(Node root) {
-        if (root == null) return;
-
-        printInorder(root.left);
-        System.out.print(root.data + " ");
-
-        printInorder(root.right);
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t > 0) {
-            String s = br.readLine();
-            Node root = buildTree(s);
-
-            Solution ob = new Solution();
-
-            ArrayList<Integer> vec = ob.topView(root);
-            for (int x : vec) System.out.print(x + " ");
-            System.out.println();
-
-            t--;
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
