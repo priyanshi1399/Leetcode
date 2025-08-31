b0247a2ee66d1e8cabc1796f2ca83837728654c3
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-08-31 07:22:43 +0530

    Time: 1 ms (98.08%) | Memory: 44.2 MB (79.50%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,38 +14,37 @@
  * }
  */
 class Solution {
-    public TreeNode solve(int [] inorder, int [] postorder, int inStart, int inEnd,int postStart,int postEnd){
+    public TreeNode solve(int [] inorder, int [] postorder, int inStart, int inEnd,int postStart,int postEnd,HashMap<Integer,Integer> map){
 
         if(inStart>inEnd){
             return null;
         }
 
         TreeNode root=new TreeNode(postorder[postEnd]);
-        int i=inStart;
-        for(;i<inEnd;i++){
-            if(inorder[i]==root.val) {
-                //searching the value in inorder
-                break;
-        }
-        }
+        int i=map.get(root.val);
 
         int leftSize=i-inStart; //tells how many element on left size
         int rightSize=inEnd-i; //tells how many element on right side
 
-        root.left=solve(inorder,postorder,inStart,i-1,postStart, postStart+leftSize-1);
-        root.right=solve(inorder,postorder,i+1,inEnd,postEnd-rightSize, postEnd-1);
+        root.left=solve(inorder,postorder,inStart,i-1,postStart, postStart+leftSize-1,map);
+        root.right=solve(inorder,postorder,i+1,inEnd,postEnd-rightSize, postEnd-1,map);
         return root;
 
     }
     public TreeNode buildTree(int[] inorder, int[] postorder) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+       
         int n=inorder.length;
+        for(int i=0;i<n;i++){
+            map.put(inorder[i],i);
+        }
         int inStart=0;
         int inEnd=n-1;
 
         int postStart=0;
         int postEnd=n-1;
 
-        return solve(inorder,postorder, inStart,inEnd,postStart,postEnd);
+        return solve(inorder,postorder, inStart,inEnd,postStart,postEnd,map);
 
     }
 }
