48603b9196ea5f253e6af2fdfadca193bddeacd6
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-09-05 11:50:46 +0530

    Time: 16 ms (6.94%) | Memory: 43.4 MB (7.51%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void convertToGraph(TreeNode curr,int parent,Map<Integer,List<Integer>> map){
+        if(curr==null){
+            return ;
+        }
+        if(parent!=-1){
+            map.computeIfAbsent(curr.val,k->new ArrayList<>()).add(parent);
+        }
+        if(curr.left!=null){
+            map.computeIfAbsent(curr.val,k->new ArrayList<>()).add(curr.left.val);
+        }
+        if(curr.right!=null){
+            map.computeIfAbsent(curr.val,k->new ArrayList<>()).add(curr.right.val);
+        }
+        convertToGraph(curr.left,curr.val,map);
+        convertToGraph(curr.right,curr.val,map);
+    }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        HashMap<Integer,List<Integer>> map=new HashMap<>();
+        convertToGraph(root,-1,map);
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> res=new ArrayList<>();
+        HashSet<Integer> visited=new HashSet<>();
+        int level=0;
+        q.add(target.val);
+        visited.add(target.val);
+        while(!q.isEmpty()){
+            if(k==0){
+                break;
+            }
+            int n=q.size();
+            while(n-->0){
+                int curr=q.poll();
+                for(int neighbour:map.getOrDefault(curr,Collections.emptyList())){
+                    if(!visited.contains(neighbour)){
+                        q.add(neighbour);
+                        visited.add(neighbour);
+                    }
+                }
+            }
+            k--;
+        }
+        
+        while(!q.isEmpty()){
+            res.add(q.peek());
+            q.poll();
+        }
+        return res;
+
+    }
+}
