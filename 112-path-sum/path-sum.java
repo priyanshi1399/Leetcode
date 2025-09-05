a8a27c63e45eb1c093cb87ad2722f27e6b693509
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-09-05 18:11:49 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (61.67%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -14,26 +14,15 @@
  * }
  */
 class Solution {
-    public boolean helper(TreeNode root,int targetSum){
-        if(root==null){
-            return false;
-        }
-        if(root.left==null && root.right==null){
-            if(targetSum==root.val){
-                return true;
-            }
-        }
-        if((helper(root.left,targetSum-root.val)) || (helper(root.right,targetSum-root.val))){
-            return true;
-        }
-        return false;
-
-    }
+   
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null){
             return false;
         }
-        return helper(root,targetSum);
+         if(root.left==null && root.right==null && root.val==targetSum){
+            return true;
+         }
+        return (hasPathSum(root.left,targetSum-root.val)) || (hasPathSum(root.right,targetSum-root.val));
 
     }
 }
