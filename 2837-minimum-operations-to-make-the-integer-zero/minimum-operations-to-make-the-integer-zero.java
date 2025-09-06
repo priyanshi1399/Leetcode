c6801a2a97545a48dfc186c46a687868c0376a16
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-09-06 06:58:22 +0530

    Time: 1 ms (43.24%) | Memory: 40.7 MB (78.38%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.java
@@ -1,6 +1,16 @@
 class Solution {
     public int makeTheIntegerZero(int num1, int num2) {
-        int t=1;
+        for(int t=1;t<=36;t++){ //computed from min value of num1 and max of num2 range
+            long val=(long)num1-t*(long)num2;
+            if(val<0){
+                break;
+            }
+            if(Long.bitCount(val)<=t && t<=val) { //range is calculated by formula 2 powers of t 
+                    return t;
+            }
+        }
+        return -1;       
+       /* int t=1;
         while(true){
             long val=(long)num1-t*(long)num2;
             if(val<0){
@@ -10,7 +20,7 @@ class Solution {
                     return t;
             }
             t++;
-        }
+        }*/
        
     }
 }
