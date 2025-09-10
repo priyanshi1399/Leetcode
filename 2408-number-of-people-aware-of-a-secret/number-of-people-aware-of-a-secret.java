c8bdb285b8476a8b7938cfd9fa69b5fbb342c4e1
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-09-10 13:45:47 +0530

    Time: 19 ms (12.61%) | Memory: 41 MB (50.45%) - LeetSync

--- 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
+++ 2408-number-of-people-aware-of-a-secret/number-of-people-aware-of-a-secret.java
@@ -1,33 +1,25 @@
 class Solution {
     private final int mod=1_000_000_007;
-    public int solve(int day, int forget,int delay,int [] dp){
-        if(day==1){
-            return 1; //only 1 person knows the secret
-        }
-          if(dp[day]!=-1){
-            return dp[day];
-        }
-        
-      
-        long result=0;
-        for(int prev=day-forget+1;prev<=day-delay;prev++){ //nth day  how nmany people knows the secret
-        // try out by chceking day-1 to day-6
-            if(prev>0){ //should not be negative
-                result=(result+solve(prev,forget,delay,dp))%mod;
-            }
-        }
-        return dp[day]=(int)result;
-    }
     public int peopleAwareOfSecret(int n, int delay, int forget) {
-        int total=0;
         int [] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        for(int day=n-forget+1;day<=n;day++){
-            if(day>0){
-            total=(total+solve(day,forget,delay,dp))%mod; //we are calculating the previous problem
+        dp[1]=1; //for 1 people
+        for(int day=2;day<=n;day++){
+            long count=0;
+            for(int prev=day-forget+1;prev<=day-delay;prev++){
+                if(prev>0){
+                count=(count+dp[prev])%mod;
+                }
             }
+            dp[day]=(int)count;
         }
 
-        return total;
+        //calculate the answer
+        int result=0;
+        for(int day=n-forget+1;day<=n;day++){
+            if(day>0){
+            result=(result+dp[day])%mod;
+            }
+        }
+        return result;
     }
 }
