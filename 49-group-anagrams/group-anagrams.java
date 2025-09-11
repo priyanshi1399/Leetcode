dd1a93f1edbc41aa6cbbe461e833bb92966c0c8f
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-09-11 17:24:53 +0530

    Time: 8 ms (38.20%) | Memory: 48.3 MB (23.27%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,19 +3,25 @@ class Solution {
         List<List<String>> res=new ArrayList<>();
         HashMap<String,List<String>> map=new HashMap<>();
         for(String word:strs){ //traversing to the every String in strs array
-            String sortedString=SortTheString(word);
-            if(!map.containsKey(sortedString)){
-                //if its not in the list of keys
-                map.put(sortedString,new ArrayList<>());
-            }
-            //if exists then it will get the key and add the word to it
+            String sortedString=getSortedString(word);
+            map.putIfAbsent(sortedString,new ArrayList<>());//it creates new ArrayList if not present
+            //if present then it will not create new ArrayList
             map.get(sortedString).add(word);
         }
         return new ArrayList<>(map.values());
     }
-    private String SortTheString(String str){
-        char [] charArray=str.toCharArray(); //convert to charArray
-        Arrays.sort(charArray);
-        return new String(charArray); //returning String after sorting it
+    private String getSortedString(String str){
+        int [] indexArray=new int[26];
+        for(int i=0;i<str.length();i++){
+            indexArray[str.charAt(i)-'a']++;
+        }
+        StringBuilder combined=new StringBuilder();
+        for(int i=0;i<26;i++){
+            if(indexArray[i]!=0){
+                int num=indexArray[i];
+                combined.append(String.valueOf((char)(i+'a')).repeat(num));
+            }
+        }
+        return combined.toString();
     }
 }
