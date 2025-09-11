6fcf134211911cbc218f8c76ccb60e07fc6ea0e3
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-09-11 17:04:17 +0530

    Time: 6 ms (98.79%) | Memory: 48.3 MB (28.60%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,33 +1,21 @@
 class Solution {
-    public String sorted(String str){
-       int [] indexArray=new int[26];
-       for(int i=0;i<str.length();i++){
-        indexArray[str.charAt(i)-'a']++;
-       }
-
-       //combining those character by index it will be in sorted Array
-       StringBuilder sortedString=new StringBuilder();
-       for(int i=0;i<26;i++){
-        if(indexArray[i]!=0){
-            int num=indexArray[i];
-            sortedString.append(String.valueOf((char)(i+'a')).repeat(indexArray[i]));
-       }
-    }
-    return sortedString.toString();
-    }
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map=new HashMap<>();
-        for(int i=0;i<strs.length;i++){
-            String sortedStr=sorted(strs[i]);
-            map.putIfAbsent(sortedStr,new ArrayList<>());
-            map.get(sortedStr).add(strs[i]);
-
-        }
-
         List<List<String>> res=new ArrayList<>();
-        for(Map.Entry<String,List<String>> entry:map.entrySet()){
-            res.add(entry.getValue());
+        HashMap<String,List<String>> map=new HashMap<>();
+        for(String word:strs){
+            String sortedString=SortTheString(word);
+            if(!map.containsKey(sortedString)){
+                //if its not in the list of keys
+                map.put(sortedString,new ArrayList<>());
+            }
+            //if exists then it will get the key and add the word to it
+            map.get(sortedString).add(word);
         }
-        return res;
+        return new ArrayList<>(map.values());
+    }
+    private String SortTheString(String str){
+        char [] charArray=str.toCharArray(); //convert to charArray
+        Arrays.sort(charArray);
+        return new String(charArray); //returning String after sorting it
     }
 }
