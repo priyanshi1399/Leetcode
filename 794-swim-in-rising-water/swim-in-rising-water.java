67aaa0ef370f40422631b9cfb679919cf8eed51a
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-10-08 04:51:07 +0530

    Time: 2 ms (98.03%) | Memory: 44.6 MB (47.57%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,47 +1,44 @@
 class Solution {
-    public final int [][] direction={{-1,0},{0,1},{1,0},{0,-1}};
-    public boolean isSafe(int x,int y,int [][] grid){
-        if(x<0 || y<0 || x>=grid.length || y>=grid.length){
+    private boolean [][] visited;
+    private final int [][] directions={{0,-1},{0,1},{1,0},{-1,0}};
+    int n;
+    public int swimInWater(int[][] grid) {
+        n=grid.length;
+        int low=0;
+        int high=n*n;
+        int result=0;
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            visited=new boolean[n][n];
+            if(isPossibleToSwim(mid,grid,0,0)){
+                result=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return result;
+    }
+    public boolean isPossibleToSwim(int mid,int [][] grid, int i, int j){
+
+        
+        if(i<0 || j<0 || i>=grid.length || j>=grid.length || grid[i][j]>mid || visited[i][j]){
             return false;
         }
-        return true;
-    }
-    public int swimInWater(int[][] grid) {
-        int n=grid.length;
-        int [][] result=new int[n][n];
-        for(int [] row:result){
-            Arrays.fill(row,Integer.MAX_VALUE);
+
+        visited[i][j]=true;
+        if(i==n-1 && j==n-1){
+            return true;
         }
-        result[0][0]=0;
-
-
-        PriorityQueue<int[]> pq=new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
-
-        pq.add(new int[]{grid[0][0],0,0});
-        while(!pq.isEmpty()){
-            int [] curr=pq.poll();
-            int diff=curr[0];
-            int x=curr[1];
-            int y=curr[2];
-            if(x==n-1 & y==n-1){
-                return diff;
-            }
-            for(int[] dir:direction){
-                int x_=x+dir[0];
-                int y_=y+dir[1];
-                if(isSafe(x_,y_,grid)){
-                    int newDiff=Math.max(grid[x_][y_],diff);
-
-
-                    if(newDiff<result[x_][y_]){
-                        result[x_][y_]=newDiff;
-                        pq.add(new int []{newDiff,x_,y_});
-                    }
-                }
-            }
-         
+        for(int [] dir:directions){
+            int i_=i+dir[0];
+            int j_=j+dir[1];
+        if(isPossibleToSwim(mid,grid,i_,j_)){
+            return true;
         }
-        return result[n-1][n-1];
-
     }
+    return false;
+}
 }
