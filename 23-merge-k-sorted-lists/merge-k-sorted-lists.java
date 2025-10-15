6a91ef76a90064f440f933cc08bf0bb594fc260c
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-10-15 18:00:15 +0530

    Time: 2 ms (87.86%) | Memory: 44.4 MB (66.36%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,28 +9,45 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0 || lists==null){
+    public ListNode partitionAndMergeList(int start,int end,ListNode [] lists){
+        if(start>end){
             return null;
         }
-        PriorityQueue<ListNode> pq=new PriorityQueue<>(lists.length,(a,b)->Integer.compare(a.val,b.val));
-        for(ListNode node:lists){
-            if(node!=null){
-                pq.add(node);
-            }
+        if(start==end){
+            return lists[start];
+        }
+        int mid=start+(end-start)/2;
+        ListNode L1=partitionAndMergeList(start,mid,lists);
+        ListNode L2=partitionAndMergeList(mid+1,end,lists);
+
+        return mergeTwoSortedList(L1,L2);
+    }
+
+    public ListNode mergeTwoSortedList(ListNode l1,ListNode l2){
+        if(l1==null){
+            return l2;
+        }
+        if(l2==null){
+            return l1;
         }
 
-        ListNode res=new ListNode(-1);
-        ListNode temp=res;
-        while(!pq.isEmpty()){
-            temp.next=pq.poll();
-            temp=temp.next;
-            if(temp.next!=null){
-                pq.add(temp.next);
-            }
+        if(l1.val<=l2.val){
+            l1.next=mergeTwoSortedList(l1.next,l2);
+            return l1;
         }
-        return res.next;
+        else{
+            l2.next=mergeTwoSortedList(l1,l2.next);
+            return l2;
+        }
+        
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        int k=lists.length;
+        if(k==0 || lists==null){
+            return null;
+        }
 
+        return partitionAndMergeList(0,k-1,lists);
 
     }
 }
