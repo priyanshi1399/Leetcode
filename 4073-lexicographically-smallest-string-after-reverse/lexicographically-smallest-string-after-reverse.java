e0851b6f84bbdc9d2b8fef7cb2595e1c83c849c8
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-10-26 06:06:21 +0530

    Time: 105 ms (50.00%) | Memory: 45.4 MB (100.00%) - LeetSync

--- 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
+++ 4073-lexicographically-smallest-string-after-reverse/lexicographically-smallest-string-after-reverse.java
@@ -3,29 +3,30 @@ class Solution {
         int n=s.length();
         String ans=s;
         for(int i=0;i<n;i++){
-            String firstPart=s.substring(0,i+1);
-            String secondPart=s.substring(i+1,n);
-            StringBuilder str=new StringBuilder(firstPart);
-            String toCheck=str.reverse().toString();
-            String result=toCheck+secondPart;
-            if(result.compareTo(ans)<0){
-                    ans=result;
+            String firstPart=s.substring(0,i);
+            firstPart=rev(firstPart);
+            String secondPart=s.substring(i);
+            String result=firstPart+secondPart;
+            if(ans.compareTo(result)>0){
+                ans=result;
             }
-
         }
-        StringBuilder str2=new StringBuilder();
-        str2.reverse();
-        for(int i=n-1;i>=0;i--){
-            String firstPart=s.substring(i);//starting index i ending last
-            String secondPart=s.substring(0,i);
-            StringBuilder str=new StringBuilder(firstPart);
-            String toCheck=str.reverse().toString();
-            String result=secondPart+toCheck;
-            if(result.compareTo(ans)<0){
-                    ans=result;
-            }
 
+          for(int i=n-1;i>=0;i--){
+            String firstPart=s.substring(i);
+            firstPart=rev(firstPart);
+            String secondPart=s.substring(0,i);
+            String result=secondPart+firstPart;
+            if(ans.compareTo(result)>0){
+                ans=result;
+            }
         }
         return ans;
+    }
+
+    public String rev(String s){
+        StringBuilder str=new StringBuilder(s);
+        str.reverse();
+        return str.toString();
     }
 }
