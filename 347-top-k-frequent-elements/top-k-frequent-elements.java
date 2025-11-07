d193b0bed3be4af4998cc256fb23cce69d155332
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2025-11-07 19:48:59 +0530

    Time: 14 ms (59.28%) | Memory: 47.6 MB (79.18%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,39 +1,31 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n=nums.length;
         HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
         for(int i=0;i<n;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        //there can be max of elements will be nums.length any number can come maximum of length times
 
-        List<Integer>[] bucket=new List[n+1]; //max of nums.length elements
-        //inside this Listof Arrays we will have arrayList bcz there can be maore element which can have same frequency
-        for(int i=0;i<n+1;i++){
-            bucket[i]=new ArrayList<>();
-        }
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->{
+            int val1=a[0];
+            int val2=b[0];
 
-        //preparing one bucket with freq as the index and value as the key
+            return val1==val2?Integer.compare(a[1],b[1]):Integer.compare(val2,val1);
+        });
+
         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
             int freq=entry.getValue();
-            int number=entry.getKey();
-
-            bucket[freq].add(number); //like 1 ,2 --> 2 times then at index 2 {1,2}
-
+            int key=entry.getKey();
+            pq.add(new int []{freq,key});
         }
-
-        List<Integer> res=new ArrayList<>();
-        for(int i=n ;i>=0 && res.size()<k;i--){ //it will fill k elements from last we can take inner loop also
-            if(!bucket[i].isEmpty()){
-                res.addAll(bucket[i]);
-            }
-        }
-
         int [] answer=new int[k];
-        for(int i=0;i<k;i++){
-            answer[i]=res.get(i);
+        int idx=0;
+        while(k-->0){
+            int [] curr=pq.poll();
+            answer[idx++]=curr[1];
         }
+
         return answer;
-    
+
     }
 }
