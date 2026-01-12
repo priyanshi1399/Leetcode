9d1daef256c2d958d8b6aedb357c720cdae8a71f
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-01-13 04:57:56 +0530

    Time: 1 ms (95.11%) | Memory: 45.2 MB (15.59%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -9,8 +9,10 @@ class Solution {
             int start_jth=points[i+1][0];
             int end_jth=points[i+1][1];
 
+            int startVal=(start_jth-start_ith)<0?(-1)*(start_jth-start_ith):(start_jth-start_ith);
+            int endVal=(end_jth-end_ith)<0?(-1)*(end_jth-end_ith):(end_jth-end_ith);
           
-            int maxVal=Math.max((start_jth-start_ith)<0?(-1)*(start_jth-start_ith):(start_jth-start_ith),(end_jth-end_ith)<0?(-1)*(end_jth-end_ith):(end_jth-end_ith));
+            int maxVal=Math.max(startVal,endVal);
             
             if(maxVal<0){
                 maxVal=maxVal*(-1);
