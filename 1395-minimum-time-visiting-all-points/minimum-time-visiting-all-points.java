e00bf1cc07c50b8199e7773568987a3980f479d7
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-01-13 06:18:55 +0530

    Time: 1 ms (95.11%) | Memory: 45.1 MB (15.59%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.java
@@ -9,12 +9,11 @@ class Solution {
             int x2=points[i+1][0];
             int y2=points[i+1][1];
 
+            //geranlised formula
+            
             int dx=Math.abs(x1-x2);
             int dy=Math.abs(y1-y2);
-          
-            int maxVal=Math.max(dx,dy);
-            
-            count+=maxVal;
+            count+=Math.min(dx,dy)+Math.abs(dx-dy);
             
             
 
