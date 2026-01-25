2c4b50b8b87af9da871686d5f9025256a7bd8769
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-01-25 11:08:39 +0530

    Time: 16 ms (90.89%) | Memory: 276.1 MB (7.85%) - LeetSync

--- 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
+++ 4171-rotate-non-negative-elements/rotate-non-negative-elements.java
@@ -15,16 +15,19 @@ class Solution {
                 arr[index++]=nums[i];
             }
          }
+         //maintain the order first of all positive number
 
         if(arr.length==0){
             return nums;
         }
+        //for 3rd rotation and in length 2 , 3rd rotation will be same as 1. 
         k=k%arr.length;
       
     
         for(int i=0,j=0;i<n;i++){
             if(nums[i]>=0){
-                nums[i]=arr[(j++ +k)%arr.length];
+                nums[i]=arr[(j +k)%arr.length]; //key of the question very important by preserving order it helped
+                j++;
             }
         }
         return nums;
