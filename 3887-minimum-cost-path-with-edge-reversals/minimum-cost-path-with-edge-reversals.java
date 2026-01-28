f11dd2a840c40ce4e641b9b2d1f53a7f5ab06b68
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-01-28 06:28:18 +0530

    Time: 85 ms (69.55%) | Memory: 261.5 MB (69.96%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -1,39 +1,32 @@
 class Solution{
 
-    static class Pair{
-        int node;
-        int dist;
-
-        Pair(int node,int dist){
-            this.node=node;
-            this.dist=dist;
-        }
-    }
 
     public int minCost(int n, int[][] edges) {
         int len=edges.length;
-        Map<Integer,List<Pair>> adj=new HashMap<>();
-
+        List<List<int[]>> adjList=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adjList.add(new ArrayList<>());
+        }
         for(int [] edge:edges){
             int u=edge[0];
             int v=edge[1];
-            int wt=edge[2];
+            int w=edge[2];
 
-            adj.computeIfAbsent(u,k->new ArrayList<>()).add(new Pair(v,wt));
-            adj.computeIfAbsent(v,k->new ArrayList<>()).add(new Pair(u,2*wt)); //reversed edge
+            adjList.get(u).add(new int[]{v,w});
+            adjList.get(v).add(new int[]{u,2*w}); //reverse
         }
         int []result=new int[n];
       
         Arrays.fill(result,Integer.MAX_VALUE);
         
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.dist-b.dist);
-        pq.add(new Pair(0,0));
+        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
+        pq.add(new int[]{0,0});
         result[0]=0;
         while(!pq.isEmpty()){
            
-           Pair curr=pq.poll();
-           int node=curr.node;
-           int d=curr.dist;
+          int [] curr=pq.poll();
+          int d=curr[0];
+          int node=curr[1];
 
            if(d>result[node]){
             continue;
@@ -43,17 +36,13 @@ class Solution{
             return d;
            }
 
-           if(!adj.containsKey(node)){
-            continue;
-           }
-
-           for(Pair neighbor:adj.get(node)){
-            int nextNode=neighbor.node;
-            int weight=neighbor.dist;
+           for(int [] neighbor:adjList.get(node)){
+            int nextNode=neighbor[0];
+            int weight=neighbor[1];
 
             if(d+weight<result[nextNode]){
                     result[nextNode]=d+weight;
-                    pq.add(new Pair(nextNode,result[nextNode]));
+                    pq.add(new int[]{result[nextNode],nextNode});
             }
            }
 
