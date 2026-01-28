93e6cbb52ae7651d02305d7eb582167fdd73a0da
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-01-28 07:28:00 +0530

    Time: 83 ms (71.19%) | Memory: 273.7 MB (49.80%) - LeetSync

--- 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -1,8 +1,6 @@
-class Solution{
-
-
+class Solution {
     public int minCost(int n, int[][] edges) {
-        int len=edges.length;
+        
         List<List<int[]>> adjList=new ArrayList<>();
         for(int i=0;i<n;i++){
             adjList.add(new ArrayList<>());
@@ -10,45 +8,49 @@ class Solution{
         for(int [] edge:edges){
             int u=edge[0];
             int v=edge[1];
-            int w=edge[2];
+            int wt=edge[2];
 
-            adjList.get(u).add(new int[]{v,w});
-            adjList.get(v).add(new int[]{u,2*w}); //reverse
+            adjList.get(u).add(new int []{wt,v});
+            adjList.get(v).add(new int []{2*wt,u});
+
         }
-        int []result=new int[n];
-      
+
+        int [] result=new int[n];
         Arrays.fill(result,Integer.MAX_VALUE);
-        
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->Integer.compare(a[0],b[0]));
-        pq.add(new int[]{0,0});
+
         result[0]=0;
+
+        PriorityQueue<int[]> pq=new PriorityQueue<>(Comparator.comparingInt(a->a[0]));
+        pq.add(new int []{0,0});
         while(!pq.isEmpty()){
-           
-          int [] curr=pq.poll();
-          int d=curr[0];
-          int node=curr[1];
 
-           if(d>result[node]){
-            continue;
-           }
+            int [] curr=pq.poll();
+            int d=curr[0];
+            int node=curr[1];
 
-           if(node==n-1){
-            return d;
-           }
-
-           for(int [] neighbor:adjList.get(node)){
-            int nextNode=neighbor[0];
-            int weight=neighbor[1];
-
-            if(d+weight<result[nextNode]){
-                    result[nextNode]=d+weight;
-                    pq.add(new int[]{result[nextNode],nextNode});
+            if(node==n-1){
+                return d;
             }
-           }
+            if(d>result[node]){
+                continue;
+            }
 
-            
+            for(int [] neighbor:adjList.get(node)){
+
+                int dist=neighbor[0];
+                int nextNode=neighbor[1];
+
+                if(d+dist<result[nextNode]){
+                    result[nextNode]=d+dist;
+                    pq.add(new int []{result[nextNode],nextNode});
+                }
+                
+            }
+
         }
         return -1;
+
+
 
     }
 }
