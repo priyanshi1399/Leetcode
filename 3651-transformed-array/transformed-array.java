b0984a46a3a79c5a27a16dd8dbc6b8d45e2c495f
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-02-08 03:27:08 +0530

    Time: 1 ms (99.35%) | Memory: 46.8 MB (55.27%) - LeetSync

--- 3651-transformed-array/transformed-array.java
+++ 3651-transformed-array/transformed-array.java
@@ -3,13 +3,19 @@ class Solution {
         int n=nums.length;
         int [] result=new int[n];
         for(int i=0;i<n;i++){
-            if(nums[i]<0){
-                result[i]=nums[Math.abs(i+n+nums[i]%n)%n];
-            }
-            else if(nums[i]>0){
-                result[i]=nums[(i+nums[i])%n];
-            }
+        int shifts=nums[i]%n;
+
+        int newIndex=(i+shifts)%n;
+
+        if(newIndex<0){
+            newIndex+=n;
         }
+
+        result[i]=nums[newIndex];
+        }
+
         return result;
+
+
     }
 }
