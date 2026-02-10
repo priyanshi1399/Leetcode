da59bf621607b5445cfb273f8eb213193cfbb00e
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-02-10 05:33:39 +0530

    Time: 2 ms (97.84%) | Memory: 48.5 MB (11.13%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.java
@@ -14,29 +14,29 @@
  * }
  */
 class Solution {
-    public void inorder(TreeNode root,List<Integer> res){
+    public void inorder(TreeNode root, List<Integer> ans){
         if(root==null){
             return;
         }
-        inorder(root.left,res);
-        res.add(root.val);
-        inorder(root.right,res);
+        inorder(root.left,ans);
+        ans.add(root.val);
+        inorder(root.right,ans);
+
     }
-    public TreeNode solve(int left,int right, List<Integer> res){
+    public TreeNode solve(int left,int right,List<Integer> ans){
         if(left>right){
             return null;
         }
-
         int mid=left+(right-left)/2;
-        TreeNode root=new TreeNode(res.get(mid));
-        root.left=solve(left,mid-1,res);
-        root.right=solve(mid+1,right,res);
+        TreeNode root=new TreeNode(ans.get(mid));
+        root.left=solve(left,mid-1,ans);
+        root.right=solve(mid+1,right,ans);
         return root;
     }
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> res=new ArrayList<>();
-        inorder(root,res);
-        int n=res.size();
-        return solve(0,n-1,res);
+        List<Integer> ans=new ArrayList<>();
+        inorder(root,ans);
+        int n=ans.size();
+        return solve(0,n-1,ans);
     }
 }
