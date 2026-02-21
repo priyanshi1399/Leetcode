16839c5300af1849176e4fd55cb38a7d82a2867d
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-02-22 03:21:41 +0530

    Time: 8 ms (45.45%) | Memory: 42.6 MB (27.65%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,30 +1,16 @@
 class Solution {
     public int countPrimeSetBits(int left, int right) {
-        boolean [] primes=CalculateSieve(right);
+        //Since 10^6 equivalent to 2^20 bits
+        //we need just 20 bits maximum
+
+        HashSet<Integer> set=new HashSet<>(Arrays.asList(2,3,5,7,11,13,17,19));
         int count=0;
         for(int i=left;i<=right;i++){
-            int countSetBit=Integer.bitCount(i);
-            if(primes[countSetBit]==true){
-
+            int bitCount=Integer.bitCount(i);
+            if(set.contains(bitCount)){
                 count++;
             }
         }
         return count;
     }
-
-    public boolean [] CalculateSieve(int n){
-        boolean [] sieve=new boolean[n+1];
-        Arrays.fill(sieve,true);
-        sieve[0]=false;
-        sieve[1]=false;
-        for(int i=2;i<n;i++){
-            if(sieve[i]==true){
-            for(int j=i*2;j<n;j=j+i){            
-                sieve[j]=false;
-            }
-            }
-        
-    }
-    return sieve;
-    } 
 }
