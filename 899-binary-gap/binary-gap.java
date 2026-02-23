21e988bd43172051c886f5c7b7bff39e7edae0da
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-02-23 10:00:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (29.57%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -1,17 +1,13 @@
 class Solution {
     public int binaryGap(int n) {
         //there can be max 20 bits 2^10~1024 ->(10^3 )^3 ->(2^10)^3
-        int currBit=0;
         int prevBit=-1;
         int maxDist=0;
-        while(n>0){
-            if((n&1)>0){
-                
-                maxDist=prevBit!=-1?Math.max(maxDist,currBit-prevBit):maxDist;
-                prevBit=currBit;
+        for(int curr=0;curr<32;curr++){
+            if((1 & (n>>curr))>0){
+                maxDist=prevBit!=-1?Math.max(maxDist,curr-prevBit):maxDist;
+                prevBit=curr;
             }
-            currBit++;
-            n=n>>1; //0 will be appended 0 leftside 
         }
         return maxDist;
     }
