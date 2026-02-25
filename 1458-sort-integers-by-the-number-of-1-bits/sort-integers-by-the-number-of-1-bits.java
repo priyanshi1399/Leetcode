cff0310b0ae2c664027f0fa9b1bb70aa1232f95e
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-02-26 05:22:46 +0530

    Time: 47 ms (5.20%) | Memory: 47.1 MB (13.65%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        int n=arr.length;
+        int [][] result=new int[n][2];
+
+        for(int i=0;i<n;i++){
+            result[i][0]=arr[i];
+            result[i][1]=Integer.bitCount(arr[i]);
+        }
+
+        Arrays.sort(result,(a,b)->{
+            if(a[1]!=b[1]){
+                return Integer.compare(a[1],b[1]);
+            }
+            else {
+                return Integer.compare(a[0],b[0]);
+            }
+        });
+
+         for(int i=0;i<result.length;i++){
+            System.out.println(result[i][0] + " :"+result[i][1]);
+            
+        }
+
+        for(int i=0;i<result.length;i++){
+            arr[i]=result[i][0];
+        }
+        return arr;
+    }
+}
