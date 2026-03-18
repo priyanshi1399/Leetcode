0c1f06d06d12846a4ee6b8aedf1fdacc7e8a8167
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-03-18 07:05:51 +0530

    Time: 19 ms (26.32%) | Memory: 110.4 MB (94.74%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.java
@@ -3,24 +3,27 @@ class Solution {
         int m=matrix.length;
         int n=matrix[0].length;
         int maxArea=0;
+        int [] prevRow=new int[n];
         for(int row=0;row<m;row++){
+            int [] currRow=matrix[row].clone();
             for(int col=0;col<n;col++){
-                if(matrix[row][col]==1 && row>0){
-                    matrix[row][col]+=matrix[row-1][col]; //adding consecutive 1s
+                if(currRow[col]==1){
+                    currRow[col]+=prevRow[col]; //adding consecutive 1s
                 }
             }
 
             //now sort the rows in descending order
-            int [] currRow=matrix[row].clone(); //exactly copying the rows
+            int [] sortedRow=currRow.clone(); //exactly copying the rows
             //sorting in ascending
-            Arrays.sort(currRow);
+            Arrays.sort(sortedRow);
 
         //calculating Area
         for(int i=0;i<n;i++){
             int width=n-i; //box base will be like i=0base 1 i=1 base 2 but we have our array in descending so n-i;
-            int height=currRow[i];
+            int height=sortedRow[i];
             maxArea=Math.max(maxArea,width*height);
         }
+            prevRow=currRow ;// assigning prevRow to currRow
         }
         return maxArea;
     }
