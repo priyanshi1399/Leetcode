f37a55f76001bb2aafc02b15a2e9dad41349bfb3
Priyanshi Singh <46901107+priyanshi1399@users.noreply.github.com>
2026-05-31 02:11:56 +0530

    Time: 8 ms (90.47%) | Memory: 49.1 MB (56.24%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,25 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n=intervals.length;
-        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-        Stack<int []> stck=new Stack<>();
-        for(int i=0;i<n;i++){
-            if(stck.isEmpty() || intervals[i][0]>stck.peek()[1]){ //8>6
-                    stck.push(intervals[i]);
+        Arrays.sort(intervals,(a,b)->{
+            int val1=a[0];
+            int val2=b[0];
+            return Integer.compare(a[0],b[0]);
+        });
+
+        List<int []> ans=new ArrayList<>();
+        for(int [] interval:intervals){
+            if(ans.isEmpty() || ans.getLast()[1]<interval[0]){
+                ans.add(interval);
             }
             else{
-                stck.peek()[1]=Math.max(stck.peek()[1],intervals[i][1]);
+                ans.getLast()[1]=Math.max(ans.getLast()[1],interval[1]);
             }
         }
-        int [][] result=new int[stck.size()][2];
-        int index=0;
-        while(!stck.isEmpty()){
-            result[index][0]=stck.peek()[0];
-            result[index][1]=stck.peek()[1];
-            stck.pop();
-            index++;
-
-        }
-        return result;
+        return ans.toArray(new int [ans.size()][2]);
     }
 }
